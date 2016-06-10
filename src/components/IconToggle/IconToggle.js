4a2084609bf7cbf69353ed41df1a8229da5ec63b
Evgeniy Pirogov <evgeniy.pirogov@simbirsoft.com>
2016-06-10 18:10:02 +0400

    #49604 - компонент, аналогичный Material UI Toggle с индикацией состояния иконками

---
+++ src/components/IconToggle/IconToggle.js
@@ -0,0 +1,50 @@
+import React, {PropTypes} from 'react';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+
+const IconToggle = (props) => {
+  const {name, stateOnIcon, stateOffIcon} = props;
+
+  const styles = {
+    group: {
+      position: 'absolute',
+      display: 'block',
+      width: 112,
+      top: 13,
+      right: 8
+    },
+    radio: {
+      float: 'right',
+      width: 'auto',
+      marginLeft: 16
+    },
+    icon: {
+    }
+  };
+
+  return (
+    <RadioButtonGroup name={name} style={styles.group} defaultSelected="1" labelPostion= "left" >
+      <RadioButton
+        value="1"
+        style={styles.radio}
+        iconStyle={styles.icon}
+        checkedIcon={stateOnIcon}
+        uncheckedIcon={stateOnIcon}
+      />
+      <RadioButton
+        value="2"
+        style={styles.radio}
+        iconStyle={styles.icon}
+        checkedIcon={stateOffIcon}
+        uncheckedIcon={stateOffIcon}
+      />
+    </RadioButtonGroup>
+  );
+};
+
+IconToggle.propTypes = {
+  name: PropTypes.string.isRequired,
+  stateOnIcon: PropTypes.object.isRequired,
+  stateOffIcon: PropTypes.object.isRequired
+};
+
+export default IconToggle;
