68394d91d44e7a775378b91d76a4eec620417bc9
pavel.starchikov <pavel.starchikov@simbirsoft.com>
2016-10-20 12:41:20 +0400

    add search and filter bar in scrum, project page

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,3 +4,5 @@ export LoginSuccess from './LoginSuccess/LoginSuccess';
 export NotFound from './NotFound/NotFound';
 export TaskPage from './TaskPage/TaskPage';
 export TasksList from './TasksList/TasksList';
+export Scrum from './Scrum';
+export Project from './Project';
