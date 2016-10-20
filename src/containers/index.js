27e73b1c52e6073425c09c7a4b392191dc4d1863
pavel.starchikov <pavel.starchikov@simbirsoft.com>
2016-10-20 13:25:02 +0400

    renamed files

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,5 +4,5 @@ export LoginSuccess from './LoginSuccess/LoginSuccess';
 export NotFound from './NotFound/NotFound';
 export TaskPage from './TaskPage/TaskPage';
 export TasksList from './TasksList/TasksList';
-export Scrum from './Scrum';
-export Project from './Project';
+export Scrum from './Scrum/Scrum';
+export Project from './Project/Project';
