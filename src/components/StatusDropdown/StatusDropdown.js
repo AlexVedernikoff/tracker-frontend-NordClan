770d88d3aaffdc3cf1f4a54e8d76ca751dd871d4
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-05-25 17:34:10 +0400

    Make Dropdown for change task status

--- src/components/StatusDropdown/StatusDropdown.js
+++ src/components/StatusDropdown/StatusDropdown.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import cn from 'classnames';
 
 export default class StatusDropdown extends React.Component {
 
   render() {
     const css = require('./StatusDropdown.scss');
-
     return (
-      <span>
-        <span className={css.status + ' ' + css.inProgress}>Статус: <span>В процессе</span></span>
+      <span className={css.statusContainer}>
+        <span className={css.status}>Статус:</span>
+        <span className={css.statuses}>
+          <span className={cn(css.status, css.inProgress)}>В процессе</span>
+          <span className={css.dropList}>
+            <span className={css.status + ' ' + css.inProgress}>Утвержден</span>
+            <span className={css.status + ' ' + css.inProgress}>Требует внимания</span>
+            <span className={css.status + ' ' + css.inProgress}>Прерван</span>
+            <span className={css.status + ' ' + css.inProgress}>Завершен</span>
+            <span className={css.status + ' ' + css.inProgress}>Отклонен</span>
+            <span className={css.status + ' ' + css.inProgress}>Заморожен</span>
+          </span>
+        </span>
       </span>
     );
   }
