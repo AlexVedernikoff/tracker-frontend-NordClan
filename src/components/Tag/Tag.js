689c56a66ddd5f9a6044c51d3d4c327428368e4f
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-06-06 12:47:15 +0400

    Project page layout

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -8,17 +8,23 @@ const Tag = (props) => {
   const {
     name,
     create,
+    blocked,
     ...other
   } = props;
   return (
     <span {...other} className={classnames({[css.tag]: true, [css.create]: create})}>
       <span className={classnames({[css.tagPart]: true, [css.tagCreate]: create})}>{create ? <IconPlus/> : name}</span>
-      {create ? null : <span className={classnames(css.tagPart, css.tagClose)}><IconClose/></span>}
+      {
+        create ? null : <span className={classnames(css.tagPart, css.tagClose)}>
+          { blocked ? null : <IconClose/> }
+        </span>
+      }
     </span>
   );
 };
 
 Tag.propTypes = {
+  blocked: PropTypes.bool,
   create: PropTypes.bool,
   name: PropTypes.string
 };
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,164 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
+
 import * as css from './Projects.scss';
+import Tag from '../../components/Tag';
+import SelectDropdown from '../../components/SelectDropdown';
 
-const Projects = () => (
-  <div>
-    <section>
-      <h1 className={css.testClass}>Projects</h1>
-    </section>
-  </div>
-);
+export default class Projects extends Component {
+  static propTypes = {
+  }
 
-export default Projects;
+  constructor (props) {
+    super(props);
+    this.state = {
+      isSectionOpen: {
+        myTasks: true,
+        otherTasks: true
+      },
+      filterTags: [],
+      changedSprint: 'sprint1'
+    };
+  }
+
+  selectValue = (e, name) => {
+    this.setState({[name]: e});
+  }
+
+  render () {
+    return (
+      <div>
+        <section>
+          <h1>Мои проекты</h1>
+          <hr/>
+          <div className={css.filters}>
+            <SelectDropdown
+              name="filterTags"
+              multi
+              placeholder="Введите название тега..."
+              backspaceToRemoveMessage="Нажмите BackSpace для очистки поля"
+              value={this.state.filterTags}
+              onChange={(e) => this.selectValue(e, 'filterTags')}
+              noResultsText="Нет результатов"
+              options={[
+                {value: 'develop', label: 'develop'},
+                {value: 'frontend', label: 'frontend'},
+                {value: 'inner', label: 'внутренний'},
+                {value: 'commerce', label: 'коммерческий'},
+                {value: 'frontend', label: 'frontend'},
+                {value: 'backend', label: 'backend'}
+              ]}
+            />
+          </div>
+          <Row>
+            <Col xs={4}>
+              <div className={css.projectCard}>
+                <h3>
+                  <Link to="/projects/1">ПроРейтинг - HR-инструмент</Link>
+                </h3>
+                <div className={css.tags}>
+                  <Tag name="design" blocked />
+                  <Tag name="react" blocked />
+                  <Tag name="redux" blocked />
+                  <Tag name="java" blocked />
+                  <Tag name="коммерческий" blocked />
+                </div>
+                <div className={css.meta}>
+                  <span>Сроки:</span>
+                  <span>06.06.2017 - 23.12.2018</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Текущий спринт:</span>
+                  <span>Спринт №1 (06.06.2017 - 23.12.2018)</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Участников:</span>
+                  <span>5</span>
+                </div>
+              </div>
+            </Col>
+            <Col xs={4}>
+              <div className={css.projectCard}>
+                <h3>
+                  <Link to="/projects/1">MakeTalents</Link>
+                </h3>
+                <div className={css.tags}>
+                  <Tag name="frontend" blocked />
+                  <Tag name="angular.js" blocked />
+                  <Tag name="java" blocked />
+                  <Tag name="внутренний" blocked />
+                </div>
+                <div className={css.meta}>
+                  <span>Сроки:</span>
+                  <span>06.06.2017 - 23.12.2018</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Текущий спринт:</span>
+                  <span>Спринт №1 (06.06.2017 - 23.12.2018)</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Участников:</span>
+                  <span>5</span>
+                </div>
+              </div>
+            </Col>
+            <Col xs={4}>
+              <div className={css.projectCard}>
+                <h3>
+                  <Link to="/projects/1">SimTrack</Link>
+                </h3>
+                <div className={css.tags}>
+                  <Tag name="frontend" blocked />
+                  <Tag name="react" blocked />
+                  <Tag name="redux" blocked />
+                  <Tag name="node.js" blocked />
+                  <Tag name="внутренний" blocked />
+                </div>
+                <div className={css.meta}>
+                  <span>Сроки:</span>
+                  <span>06.06.2017 - 23.12.2018</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Текущий спринт:</span>
+                  <span>Спринт №1 (06.06.2017 - 23.12.2018)</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Участников:</span>
+                  <span>5</span>
+                </div>
+              </div>
+            </Col>
+            <Col xs={4}>
+              <div className={css.projectCard}>
+                <h3>
+                  <Link to="/projects/1">Qiwi-Artek</Link>
+                </h3>
+                <div className={css.tags}>
+                  <Tag name="frontend" blocked />
+                  <Tag name="react" blocked />
+                  <Tag name="redux" blocked />
+                  <Tag name="java" blocked />
+                  <Tag name="коммерческий" blocked />
+                </div>
+                <div className={css.meta}>
+                  <span>Сроки:</span>
+                  <span>06.06.2017 - 23.12.2018</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Текущий спринт:</span>
+                  <span>Спринт №1 (06.06.2017 - 23.12.2018)</span>
+                </div>
+                <div className={css.meta}>
+                  <span>Участников:</span>
+                  <span>5</span>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </section>
+      </div>
+    );
+  }
+}
+
