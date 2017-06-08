c8c950dc6676c79b005cdf27d49c2535963c11f9
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-06-08 10:49:39 +0400

    Project parent in project list

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -126,3 +126,10 @@ export const IconTime = (props) => (
     <path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/>
   </SvgIcon>
 );
+
+export const IconFolderOpen = (props) => (
+  <SvgIcon {...props}>
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"/>
+  </SvgIcon>
+);
