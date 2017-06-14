3fd589ae74207084b3595ef0945c8ccddce66ed7
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-14 18:09:25 +0400

    added modal window for file view

--- src/components/AttachedFile/AttachedFile.js
+++ src/components/AttachedFile/AttachedFile.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router';
-import { IconFileDocument, IconFilePdf, IconDelete, IconDownload, IconPlus, IconEye } from '../Icons';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router";
+import {
+  IconFileDocument,
+  IconFilePdf,
+  IconDelete,
+  IconDownload,
+  IconPlus,
+  IconEye
+} from "../Icons";
 import FileViewer from "react-file-viewer";
+import ReactModal from "react-modal";
+
+ReactModal.defaultStyles.content.left = "260px";
 
 export default class AttachedFile extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
-    }
+      isModalOpen: false
+    };
+
+    this.handleModal = this.handleModal.bind(this);
+  }
+
+  handleModal() {
+    console.log("setting modal state");
+    this.setState({ isModalOpen: !this.state.isModalOpen });
   }
 
   render() {
-
-    const css = require('./AttachedFile.scss');
+    const css = require("./AttachedFile.scss");
 
     const iconStyles = {
       width: 24,
       height: 24,
-      color: 'inherit',
-      fill: 'currentColor'
+      color: "inherit",
+      fill: "currentColor"
     };
 
-    const {
-      fileName,
-      filePath,
-      fileType
-    } = this.props;
+    const { fileName, filePath, fileType } = this.props;
 
     return (
       <li className={css.attachment}>
@@ -44,10 +55,18 @@ export default class AttachedFile extends React.Component {
               </button>
             </div>
           </div>
-          <div className={css.attachmentName}>{fileName}</div>
         </Link>
-      </li>
-    )
-  }
+        <div className={css.attachmentName} onClick={this.handleModal}>
+          {fileName}
+        </div>
 
+        <ReactModal
+          isOpen={this.state.isModalOpen}
+          contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.handleModal}>Close Modal</button>
+        </ReactModal>
+      </li>
+    );
+  }
 }
