94095a5b8c3fef33215a8c75a0d9cf6c4c6ff7dc
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-15 12:58:43 +0400

    added more example files to test

--- mocks/Files.js
+++ mocks/Files.js
@@ -1,5 +1,3 @@
-const fileDirectory = "http://localhost:8080/example_files/";
-
 const files = [
   {
     fileType: "pdf",
@@ -15,6 +13,41 @@ const files = [
     fileType: "jpg",
     fileName: "sample-file.jpg",
     filePath: require("../example_files/picture.jpg")
+  },
+  {
+    fileType: "jpg",
+    fileName: "360photo.jpg",
+    filePath: require("../example_files/360photo.jpg")
+  },
+  {
+    fileType: "avi",
+    fileName: "drop.avi",
+    filePath: require("../example_files/drop.avi")
+  },
+  {
+    fileType: "mp3",
+    fileName: "sample.mp3",
+    filePath: require("../example_files/sample.mp3")
+  },
+  {
+    fileType: "wexbim",
+    fileName: "SampleHouse.wexbim",
+    filePath: require("../example_files/sample.mp3")
+  },
+  {
+    fileType: "docx",
+    fileName: "samplespec.docx",
+    filePath: require("../example_files/SampleSpec.docx")
+  },
+  {
+    fileType: "xlsx",
+    fileName: "SimpleSpreadsheet.xlsx",
+    filePath: require("../example_files/small.mp4")
+  },
+  {
+    fileType: "csv",
+    fileName: "Total_Crime.csv",
+    filePath: require("../example_files/Total_Crime.csv")
   }
 ];
 
