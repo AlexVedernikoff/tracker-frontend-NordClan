bba7c3a43607aa6247aa37f37d729e5db9a1e6bc
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-23 16:49:23 +0400

    Auth actions

---
+++ src/actions/Authentication.js
@@ -0,0 +1,31 @@
+import * as AuthActions from '../constants/Authentication';
+import axios from 'axios';
+
+function startAuthentication() {
+  return {
+    type: AuthActions.AUTHENTICATION_START
+  };
+}
+
+function AuthenticationError(message) {
+  return {
+    type: AuthActions.AUTHENTICATION_ERROR,
+    errorMessage: message
+  };
+}
+
+export function doAuthentication(username, password) {
+  const URL = `http://sim-track.simbirsoft/api/auth/login`;
+
+  return dispatch => dispatch(startAuthentication());
+  axios
+    .post(url, { login: username, ...password })
+    .catch(error => AuthenticationError(error))
+    .then(response => {
+      if (!response) {
+        return;
+      } else {
+        console.log(response);
+      }
+    });
+}
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
 const loggerMiddleware = createLogger();
 
-export default function configureStore(preloadedState) {
+const store = function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
     applyMiddleware(thunkMiddleware, loggerMiddleware)
   );
-}
+};
+
+export default store;
+
+export const history = syncHistoryWithStore(browserHistory, store);
