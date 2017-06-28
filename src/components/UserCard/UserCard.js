48140558256ba2dc487d49ea76dea85e7855c233
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-06-28 10:31:27 +0400

    Complete User Card

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -5,13 +5,30 @@ import * as css from './UserCard.scss';
 import onClickOutside from 'react-onclickoutside';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
+import { IconSkype, IconMail, IconPhone } from '../Icons';
+
 const user = {
-  pic: 'http://lorempixel.com/200/200/people/',
-  name: 'Андрей',
-  surname: 'Юдин',
-  skype: 'woody.just@gmail.com',
-  email: 'woody.just@gmail.com',
-  phone: '+79041862212'
+  1: {
+    pic: 'http://lorempixel.com/200/200/people/',
+    name: 'Анастасия Горшкова',
+    skype: 'woody.just@gmail.com',
+    email: 'woody.just@gmail.com',
+    phone: '+79041862212'
+  },
+  2: {
+    pic: 'http://lorempixel.com/200/200/people/',
+    name: 'Максим Слепухов',
+    skype: 'maxim.slepuchov',
+    email: 'maxim.slepuchov@gmail.com',
+    phone: '+7 904 186 22 12'
+  },
+  3: {
+    pic: 'http://lorempixel.com/200/200/people/',
+    name: 'Виктор Сычев ',
+    skype: 'sychev.victor',
+    email: 'victor.sychev@simbirsoft.com',
+    phone: '+7 960 377 90 27'
+  }
 };
 
 class UserCard extends React.Component {
@@ -44,7 +61,26 @@ class UserCard extends React.Component {
         <ReactCSSTransitionGroup transitionName="animatedElement" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
           {
             this.state.visible
-            ? <div className={css.userCard}>{this.props.id}</div>
+            ? <div className={css.userCard}>
+              <div className={css.photoWrapper}>
+                <img src={user[this.props.id].pic} alt=""/>
+              </div>
+              <div className={css.name}>
+                {user[this.props.id].name}
+              </div>
+              <div className={css.meta}>
+                <span><IconSkype/></span>
+                <span><a href={`skype:${user[this.props.id].skype}?add`}>{user[this.props.id].skype}</a></span>
+              </div>
+              <div className={css.meta}>
+                <span><IconMail/></span>
+                <span><a href={`mailto:${user[this.props.id].email}`}>{user[this.props.id].email}</a></span>
+              </div>
+              <div className={css.meta}>
+                <span><IconPhone/></span>
+                <span><a href={`tel:${user[this.props.id].phone}`}>{user[this.props.id].phone}</a></span>
+              </div>
+            </div>
             : null
           }
         </ReactCSSTransitionGroup>
