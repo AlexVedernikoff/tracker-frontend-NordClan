09485c8e9090560b3f8517907a45e37ba95c178a
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-28 14:57:01 +0400

    Pagination component boilerplate

--- src/actions/Projects.js
+++ src/actions/Projects.js
@@ -1,4 +1,48 @@
-import * as ProjectActions from "../constants/Projects";
-import axios from "axios";
+import * as ProjectActions from '../constants/Projects';
+import axios from 'axios';
 import { store } from '../Router';
 import { history } from '../Router';
+
+function startProjectsReceive() {
+  return {
+    type: AuthActions.PROJECTS_RECEIVE_START
+  };
+}
+
+function ProjectsReceiveError(message) {
+  return {
+    type: AuthActions.PROJECTS_RECEIVE_ERROR,
+    errorMessage: message
+  };
+}
+
+function ProjectsReceived(projects) {
+  return {
+    type: AuthActions.PROJECTS_RECEIVE_SUCCESS,
+    data: projects
+  };
+}
+
+export function getProjects(pageSize, currentPage, tags) {
+  const URL = `/project`;
+
+  return dispatch => {
+    dispatch(startProjectsReceive());
+    axios
+      .get(
+        URL,
+        { login: username, password: password },
+        { withCredentials: true }
+      )
+      .catch(error => dispatch(AuthenticationError(error.message)))
+      .then(response => {
+        if (!response) {
+          return;
+        } else if (response.status === 200) {
+          window.localStorage.setItem('simTrackAuthToken', response.data.token);
+          dispatch(AuthenticationReceived(response.data.user));
+          history.push('/projects');
+        }
+      });
+  };
+}
---
+++ src/components/Pagination/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import classname from 'classnames';
+import * as css from './theme.scss';
+
+const Pagination = (props) => {
+  const { itemsCount, className, activePage, onItemClick, prevText, nextText } = props;
+
+  const PrevDisabled = activePage === 1;
+  const NextDisabled = activePage === itemsCount;
+
+  const handleClick = (clickOptions, e) => {
+    e.preventDefault();
+    let options = clickOptions;
+
+    if (clickOptions.isPrev) {
+      if (PrevDisabled) {
+        return;
+      }
+
+      options = {
+        ...clickOptions,
+        activePage: activePage - 1
+      };
+    }
+
+    if (clickOptions.isNext) {
+      if (NextDisabled) {
+        return;
+      }
+
+      options = {
+        ...clickOptions,
+        activePage: activePage + 1
+      };
+    }
+
+    if (onItemClick && typeof onItemClick === 'function') {
+      onItemClick(options);
+    }
+  };
+
+  const items = [];
+  for (let i = 0; i < itemsCount; i++) {
+    const index = i + 1;
+    items.push(<li key={i} className={cn('page-item', {
+          'active': activePage === index
+        })}
+        onClick={(e) => handleClick({ activePage: index }, e)}
+      >
+        <a className="page-link" href="#">{index}</a>
+      </li>);
+  }
+
+  return (
+    <ul className={cn('pagination', className)}>
+      <li className={cn('page-item', {
+        'disabled': activePage === 1
+      })} onClick={(e) => handleClick({ isPrev: true }, e)}><a className="page-link" href="#">{prevText || 'Пред.'}</a></li>
+      {
+        itemsCount && items
+      }
+      <li className={cn('page-item', {
+        'disabled': activePage === itemsCount
+      })} onClick={(e) => handleClick({ isNext: true }, e)}><a className="page-link" href="#">{nextText || 'След.'}</a></li>
+    </ul>
+  );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  className: PropTypes.string,
+  activePage: PropTypes.number.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+  prevText: PropTypes.string,
+  nextText: PropTypes.string
+};
+
+export default Pagination;
