f0f57f912358ae21fd116eef6310926b64ffe90f
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-06-28 19:18:28 +0400

    Select comment

--- src/pages/TaskPage/Comments/Comments.js
+++ src/pages/TaskPage/Comments/Comments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import classnames from 'classnames';
 
 import * as css from './Comments.scss';
 
@@ -7,7 +8,7 @@ import * as css from './Comments.scss';
 const comments = [
   {
     id: 1,
-    parentId: null,
+    parent: null,
     autorName: 'Автор Автор',
     autorId: 1,
     autorPic: 'http://lorempixel.com/56/56/people/',
@@ -16,7 +17,15 @@ const comments = [
   },
   {
     id: 2,
-    parentId: null,
+    parent: {
+      id: 3,
+      parent: null,
+      autorName: 'Максим Шотландский',
+      autorId: 1,
+      autorPic: null,
+      date: '17.03.2016',
+      text: 'Это начало ветки комментариев. И вовь вы можете лицезреть аватарку из букв, в этот раз для примера я использовал другую пару символов'
+    },
     autorName: 'Андрей Юдин',
     autorId: 1,
     autorPic: null,
@@ -25,7 +34,7 @@ const comments = [
   },
   {
     id: 3,
-    parentId: null,
+    parent: null,
     autorName: 'Максим Шотландский',
     autorId: 1,
     autorPic: null,
@@ -34,7 +43,7 @@ const comments = [
   },
   {
     id: 4,
-    parentId: null,
+    parent: null,
     autorName: 'Андрей Юдин',
     autorId: 1,
     autorPic: null,
@@ -43,13 +52,24 @@ const comments = [
   }
 ];
 
-
 export default class Comments extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {selectedComment: {}};
+  }
+
+  selectComment = (id) => {
+    const selectedComment = comments.filter((element) => {
+      return element.id === id;
+    });
+    this.setState({selectedComment: selectedComment[0]});
+    console.log(selectedComment[0].id);
+  }
 
   render () {
 
     const commentsList = comments.map((element) => {
-      console.log(element.autorPic);
       let typoAvatar = '';
       if (!element.autorPic) {
         const wordsArr = element.autorName.split(' ');
@@ -59,22 +79,29 @@ export default class Comments extends Component {
         typoAvatar.toLocaleUpperCase();
       }
       return (
-        <li className={css.commentContainer} key={element.id}>
-          <div className={css.commentContainer}>
-            <div className={css.comment}>
-              <div className={css.ava}>
-                {
-                  element.autorPic
-                  ? <img src={element.autorPic}/>
-                  : typoAvatar
-                }
-              </div>
-              <div className={css.commentBody}>
-                <div className={css.commentMeta}><Link to={`#${element.id}`}>{element.autorName}</Link>, {element.date}</div>
-                <div className={css.commentText}>{element.text}</div>
-                <div className={css.commentAction}>
-                  <Link to="#">Ответить</Link>
+        <li className={classnames({[css.commentContainer]: true, [css.selected]: element.id === this.state.selectedComment.id})} key={element.id}>
+          <div className={css.comment}>
+            <div className={css.ava}>
+              {
+                element.autorPic
+                ? <img src={element.autorPic}/>
+                : typoAvatar
+              }
+            </div>
+            <div className={css.commentBody}>
+              <div className={css.commentMeta}><Link to={`#${element.id}`}>{element.autorName}</Link>, {element.date}</div>
+              {
+                element.parent
+                ? <div className={css.commentQuote} onClick={() => this.selectComment(element.parent.id)}>
+                  <a className={css.commentQuoteAutor}>{element.parent.autorName},</a>
+                  <span className={css.commentQuoteDate}>{element.parent.date}:</span>
+                  «{element.parent.text}»
                 </div>
+                : null
+              }
+              <div className={css.commentText}>{element.text}</div>
+              <div className={css.commentAction}>
+                <Link to="#">Ответить</Link>
               </div>
             </div>
           </div>
