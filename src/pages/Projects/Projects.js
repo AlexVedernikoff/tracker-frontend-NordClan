e7c6c9eac69978027318fe705a5372adddbd62fa
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-28 16:48:54 +0400

    connect Projects component to store

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
+import { connect } from 'react-redux';
 
 import * as css from './Projects.scss';
 import SelectDropdown from '../../components/SelectDropdown';
@@ -10,134 +11,11 @@ import DatepickerDropdown from '../../components/DatepickerDropdown';
 import Input from '../../components/Input';
 import ProjectCard from '../../components/ProjectCard';
 import StatusCheckbox from './StatusCheckbox';
-import Pagination from "../../components/Pagination";
+import Pagination from '../../components/Pagination';
 
-// Mocks
+import { getProjects } from '../../actions/Projects';
 
-const projects = [];
-const getRandomString = arr => {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-
-const getSomeRandomString = arr => {
-  const start = Math.floor(Math.random() * arr.length);
-  const end = arr.length - start;
-  return arr.splice(start, end);
-};
-
-const mockTags = [];
-
-for (let i = 0; i < 15; i++) {
-  projects.push({
-    id: i,
-    name: getRandomString([
-      'MakeTalents',
-      'Киви-Банк - {Всем',
-      'Грехов И.В.- "BookReview"',
-      'SimTrack',
-      'Qiwi-Artek',
-      'ПроРейтинг - HR-инструмент',
-      'Корпоративные сайты SimbirSoft',
-      'Аудит информационной безопасности',
-      'Онлайн-опросы (ООО "Top of Mind Research")',
-      'ИП Хабибрахманов Р.Р. - ФЛЭТ CRM Битрикс24'
-    ]),
-    tags: mockTags.concat(
-      getSomeRandomString([
-        'frontend',
-        'java',
-        'C++',
-        'php',
-        'angular.js',
-        'angular',
-        'react'
-      ]),
-      getRandomString(['2017', '2016', '2015']),
-      getRandomString(['внутренний', 'коммерческий'])
-    ),
-    dateStart: '06.06.2017',
-    dateEnd: '26.12.2017',
-    activeSprint: {
-      name: getRandomString([
-        'Спринт №1',
-        'Спринт №2',
-        'Спринт №3',
-        'Спринт №4'
-      ]),
-      dateStart: '06.06.2017',
-      dateEnd: '26.12.2017'
-    },
-    status: getRandomString(['INPROGRESS', 'INHOLD', 'FINISHED']),
-    members: getRandomString(['5', '15', '20'])
-  });
-}
-
-const projectFolder = {
-  name: 'MakeTalents new UI',
-  children: [
-    {
-      id: 0,
-      name: 'MakeTalents - Android',
-      tags: ['android', 'angular.js', 'frontend', '2016', 'внутренний'],
-      dateStart: '06.06.2017',
-      dateEnd: '26.12.2017',
-      activeSprint: {
-        name: 'Спринт №36',
-        dateStart: '06.06.2017',
-        dateEnd: '26.12.2017'
-      },
-      status: 'INPROGRESS',
-      members: '20'
-    },
-    {
-      id: 1,
-      name: 'MakeTalents - iOS',
-      tags: ['iOS', 'angular.js', 'frontend', '2016', 'внутренний'],
-      dateStart: '06.06.2017',
-      dateEnd: '26.12.2017',
-      activeSprint: {
-        name: 'Спринт №36',
-        dateStart: '06.06.2017',
-        dateEnd: '26.12.2017'
-      },
-      status: 'INPROGRESS',
-      members: '20'
-    },
-    {
-      id: 1,
-      name: 'MakeTalents - web',
-      tags: ['web', 'angular.js', 'frontend', '2016', 'внутренний'],
-      dateStart: '06.06.2017',
-      dateEnd: '26.12.2017',
-      activeSprint: {
-        name: 'Спринт №36',
-        dateStart: '06.06.2017',
-        dateEnd: '26.12.2017'
-      },
-      status: 'INPROGRESS',
-      members: '20'
-    }
-  ]
-};
-
-const projectList = projects.map((element, i) =>
-  <ProjectCard key={i} project={element} />
-);
-const projectFolderNode = (
-  <div style={{ marginBottom: 24 }} key={321654}>
-    <h2 style={{ marginBottom: 32 }}>
-      {/*<IconFolderOpen style={{color: 'silver'}} />*/} {projectFolder.name}
-    </h2>
-    <ProjectCard project={projectFolder.children[0]} isChild key={1} />
-    <ProjectCard project={projectFolder.children[1]} isChild key={2} />
-    <ProjectCard project={projectFolder.children[2]} isChild key={3} />
-  </div>
-);
-projectList[3] = projectFolderNode;
-
-export default class Projects extends Component {
-  static propTypes = {};
-
+class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -163,8 +41,13 @@ export default class Projects extends Component {
     this.setState({ [name]: e });
   };
 
-  handlePaginationClick = (e) => {
+  handlePaginationClick = e => {
     e.preventDefault;
+  };
+
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch(getProjects(25, 1, ''));
   }
 
   render() {
@@ -235,10 +118,8 @@ export default class Projects extends Component {
               </Col>
             </Row>
           </div>
-          <div>
-            {projectList}
-          </div>
-          <hr/>
+          <div />
+          <hr />
           {2 > 1
             ? <Pagination
                 itemsCount={3}
@@ -251,3 +132,12 @@ export default class Projects extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isReceiving: state.Projects.isReceiving,
+    projects: state.Projects.isReceiving
+  };
+};
+
+export default connect(mapStateToProps)(Projects);
