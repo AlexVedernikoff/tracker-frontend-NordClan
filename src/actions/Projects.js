09a1b843ec8eecd7905acdc41d1aceda0d987123
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-06-28 12:18:25 +0400

    projects integration initial commit

---
+++ src/actions/Projects.js
@@ -0,0 +1,4 @@
+import * as ProjectActions from "../constants/Projects";
+import axios from "axios";
+import { store } from '../Router';
+import { history } from '../Router';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import Auth from './Authentication';
+import Projects from "./Projects";
 
-const rootReducer = combineReducers({ Auth, routing: routerReducer });
+const rootReducer = combineReducers({ Auth, Projects, routing: routerReducer });
 
 export default rootReducer;
