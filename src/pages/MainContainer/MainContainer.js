50d1585c30b201847812b8072aae17bfbd1febf5
Pavel Dadykin <pavel.dadykin@simbirsoft.com>
2017-07-04 17:00:00 +0400

    Fixed error with "Two HTML5 backends "

--- src/pages/MainContainer/MainContainer.js
+++ src/pages/MainContainer/MainContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContextProvider } from 'react-dnd';
 import * as css from './MainContainer.scss';
 
 export default class MainContainer extends Component {
@@ -9,9 +11,11 @@ export default class MainContainer extends Component {
 
   render () {
     return (
-      <div id="app">
-        {this.props.children}
-      </div>
+      <DragDropContextProvider backend={HTML5Backend}>
+        <div id="app">
+          {this.props.children}
+        </div>
+      </DragDropContextProvider>
     );
   }
 }
