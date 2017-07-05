68416a9fe6a410fd869e031324abd77c03898b55
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-07-05 11:48:24 +0400

    settings fixes

--- src/pages/ProjectPage/Settings/Settings.js
+++ src/pages/ProjectPage/Settings/Settings.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
 
 import * as css from './Settings.scss';
 import SprintCard from '../../../components/SprintCard';
@@ -38,14 +39,14 @@ const sprints = [
   }
 ];
 
-export default class Settings extends Component {
-  static propTypes = {
-  }
-
+class Settings extends Component {
   render () {
-
     const sprintList = sprints.map((element, i) => {
-      return <Col xs={3} key={i}><SprintCard sprint={element}/></Col>;
+      return (
+        <Col xs={3} key={i}>
+          <SprintCard sprint={element} />
+        </Col>
+      );
     });
 
     return (
@@ -55,169 +56,281 @@ export default class Settings extends Component {
           <Col xs={9} xsOffset={3}>
             <Row>
               <Col xs>
-                <h4><div className={css.cell}>Develop</div></h4>
+                <h4>
+                  <div className={css.cell}>Develop</div>
+                </h4>
               </Col>
               <Col xs>
-                <h4><div className={css.cell}>Back</div></h4>
+                <h4>
+                  <div className={css.cell}>Back</div>
+                </h4>
               </Col>
               <Col xs>
-                <h4><div className={css.cell}>Front</div></h4>
+                <h4>
+                  <div className={css.cell}>Front</div>
+                </h4>
               </Col>
               <Col xs>
-                <h4><div className={css.cell}>Code Review</div></h4>
+                <h4>
+                  <div className={css.cell}>Code Review</div>
+                </h4>
               </Col>
               <Col xs>
-                <h4><div className={css.cell}>QA</div></h4>
+                <h4>
+                  <div className={css.cell}>QA</div>
+                </h4>
               </Col>
               <Col xs>
-                <h4><div className={css.cell}>Unbillable</div></h4>
+                <h4>
+                  <div className={css.cell}>Unbillable</div>
+                </h4>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className={css.memberRow}>
           <Col xs={3}>
-            <Link to="/users/123"><div className={classnames(css.cell, css.memberColumn)}>Бронислав Пупков</div></Link>
+            <Link to="/users/123">
+              <div className={classnames(css.cell, css.memberColumn)}>
+                Бронислав Пупков
+              </div>
+            </Link>
           </Col>
           <Col xs={9}>
             <Row>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className={css.memberRow}>
           <Col xs={3}>
-            <Link to="/users/123"><div className={classnames(css.cell, css.memberColumn)}>Александра Одиннадцатиклассница</div></Link>
+            <Link to="/users/123">
+              <div className={classnames(css.cell, css.memberColumn)}>
+                Александра Одиннадцатиклассница
+              </div>
+            </Link>
           </Col>
           <Col xs={9}>
             <Row>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className={css.memberRow}>
           <Col xs={3}>
-            <Link to="/users/123"><div className={classnames(css.cell, css.memberColumn)}>Никита Джугашвили</div></Link>
+            <Link to="/users/123">
+              <div className={classnames(css.cell, css.memberColumn)}>
+                Никита Джугашвили
+              </div>
+            </Link>
           </Col>
           <Col xs={9}>
             <Row>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className={css.memberRow}>
           <Col xs={3}>
-            <Link to="/users/123"><div className={classnames(css.cell, css.memberColumn)}>Николай Липотин</div></Link>
+            <Link to="/users/123">
+              <div className={classnames(css.cell, css.memberColumn)}>
+                Николай Липотин
+              </div>
+            </Link>
           </Col>
           <Col xs={9}>
             <Row>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
             </Row>
           </Col>
         </Row>
         <Row className={css.memberRow}>
           <Col xs={3}>
-            <Link to="/users/123"><div className={classnames(css.cell, css.memberColumn)}>Андрей Юдин</div></Link>
+            <Link to="/users/123">
+              <div className={classnames(css.cell, css.memberColumn)}>
+                Андрей Юдин
+              </div>
+            </Link>
           </Col>
           <Col xs={9}>
             <Row>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
               <Col xs>
-                <label className={css.cell}><Checkbox/></label>
+                <label className={css.cell}>
+                  <Checkbox />
+                </label>
               </Col>
             </Row>
           </Col>
         </Row>
-        <Button text="Добавить участника" type="primary" style={{marginTop: 16}} icon="IconPlus"/>
-        <hr/>
+        <Button
+          text="Добавить участника"
+          type="primary"
+          style={{ marginTop: 16 }}
+          icon="IconPlus"
+        />
+        <hr />
         <h2>Спринты / Фазы</h2>
         <Row>
           {sprintList}
         </Row>
-        <Button text="Создать спринт" type="primary" style={{marginTop: 16}} icon="IconPlus"/>
+        <Button
+          text="Создать спринт"
+          type="primary"
+          style={{ marginTop: 16 }}
+          icon="IconPlus"
+        />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  sprints: state.Project.project.sprintList
+});
+
+Settings.propTypes = {
+  sprints: PropTypes.array.isRequired
+};
+
+export default connect(mapStateToProps)(Settings);
