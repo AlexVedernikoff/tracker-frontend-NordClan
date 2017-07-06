f28b3f1f29576772950b5cfcaddca50379173952
Mikhail Mikheev <mikhail.miheev@simbirsoft.com>
2017-07-06 15:56:08 +0400

    Add notification

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { IconError } from '../Icons';
+import * as css from './Notification.scss';
 
 class Notification extends Component {
   constructor (props) {
@@ -8,8 +9,11 @@ class Notification extends Component {
 
   render () {
     return (
-      <div>
-        <IconError style={{ width: 100, height: 100 }} />
+      <div className={css.Notification}>
+        <IconError style={{ width: 50, height: 50 }} />
+        <p className={css.NotificationMessage}>
+          {this.props.message || "Lorem Ipsum Ekam Saedi Neppah"}
+        </p>
       </div>
     );
   }
--- src/pages/NotificationContainer/NotificationContainer.js
+++ src/pages/NotificationContainer/NotificationContainer.js
@@ -3,15 +3,18 @@ import * as css from "./NotificationContainer.scss";
 import Notification from "../../components/Notification";
 
 class NotificationContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div className={css.NotificationContainer}>
-
+        <Notification />
       </div>
     );
   }
-
 }
+
 
 export default NotificationContainer;
