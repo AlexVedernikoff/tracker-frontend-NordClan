00ab5f6a314beaa6a1b715f315b79f4c5e67850e
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-08-16 16:12:05 +0400

    Some revisions

--- src/pages/InnerContainer/AppHead/Playlist/Playlist.js
+++ src/pages/InnerContainer/AppHead/Playlist/Playlist.js
@@ -5,7 +5,7 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import onClickOutside from 'react-onclickoutside';
 import ReactTooltip from 'react-tooltip';
 
-import { IconPause, IconPlay } from '../../../../components/Icons';
+import { IconPause, IconPlay, IconLock, IconArrowDown, IconArrowUp, IconBook, IconList, IconLaptop, IconCall, IconPlane, IconTime } from '../../../../components/Icons';
 import * as css from './Playlist.scss';
 
 class Playlist extends Component {
@@ -37,13 +37,16 @@ class Playlist extends Component {
     this.setState({isPlaylistOpen: !this.state.isPlaylistOpen});
   }
 
+  handleShowOther = () => {
+    this.setState({isNotMineTasksShow: !this.state.isNotMineTasksShow});
+  }
+
   handleChangeItem = (name, e) => {
     this.setState({times: {...this.state.times, [name]: e.target.value}});
-    console.log(this.state);
   }
 
   render () {
-    const { isPlaylistOpen } = this.state;
+    const { isPlaylistOpen, isNotMineTasksShow } = this.state;
     const tasks
     = <div>
         <div className={classnames(css.listTask, css.task)}>
@@ -52,8 +55,10 @@ class Playlist extends Component {
           </div>
           <div className={css.taskNameWrapper}>
             <div className={css.taskTitle}>
-              <div className={css.taskSubtitle}>
-                ST-48
+              <div className={css.meta}>
+                <span>ST-48</span>
+                <span>ПроРейтинг - HR-инструмент</span>
+                <span>Develop</span>
               </div>
               <div className={css.taskName}>
                 UI: Страница задачи. Не хватает кнопки Создания задачи со страницы задачи
@@ -65,8 +70,7 @@ class Playlist extends Component {
               <input key="item1" type="text" onChange={(e) => this.handleChangeItem('item1', e)} value={this.state.times.item1}/>
             </div>
             <div className={css.other}>
-              <span data-tip="Потрачено" data-place="bottom">52</span>
-              <span data-tip="Запланировано" data-place="bottom">14</span>
+              <span data-tip="Потрачено" data-place="bottom">52</span> / <span data-tip="Запланировано" data-place="bottom">14</span>
             </div>
           </div>
         </div>
@@ -76,8 +80,10 @@ class Playlist extends Component {
           </div>
           <div className={css.taskNameWrapper}>
             <div className={css.taskTitle}>
-              <div className={css.taskSubtitle}>
-                ST-60
+              <div className={css.meta}>
+                <span>ST-48</span>
+                <span>SimTrack</span>
+                <span>Develop</span>
               </div>
               <div className={css.taskName}>
                 UI: Добавить смену приоритета
@@ -89,8 +95,7 @@ class Playlist extends Component {
               <input key="item2" type="text" onChange={(e) => this.handleChangeItem('item2', e)} value={this.state.times.item2}/>
             </div>
             <div className={css.other}>
-              <span data-tip="Всего" data-place="bottom">52</span>
-              <span data-tip="За эту неделю" data-place="bottom">14</span>
+              <span data-tip="Потрачено" data-place="bottom">52</span> / <span data-tip="Запланировано" data-place="bottom">14</span>
             </div>
           </div>
         </div>
@@ -100,8 +105,10 @@ class Playlist extends Component {
           </div>
           <div className={css.taskNameWrapper}>
             <div className={css.taskTitle}>
-              <div className={css.taskSubtitle}>
-                ST-3214
+              <div className={css.meta}>
+                <span>ST-3214</span>
+                <span>SimTrack</span>
+                <span>Develop</span>
               </div>
               <div className={css.taskName}>
                 UI: Список Проектов. Фильтрация по тэгам
@@ -113,8 +120,61 @@ class Playlist extends Component {
               <input key="item3" type="text" onChange={(e) => this.handleChangeItem('item3', e)} value={this.state.times.item3}/>
             </div>
             <div className={css.other}>
-              <span data-tip="Всего" data-place="bottom">52</span>
-              <span data-tip="За эту неделю" data-place="bottom">14</span>
+              <span data-tip="Потрачено" data-place="bottom">52</span> / <span data-tip="Запланировано" data-place="bottom">14</span>
+            </div>
+          </div>
+        </div>
+      </div>;
+
+    const notMineTasks
+    = <div>
+        <div className={classnames(css.listTask, css.task)}>
+          <div className={classnames(css.actionButton, css.locked)}>
+            <IconLock style={{width: '1.5rem', height: '1.5rem'}} />
+          </div>
+          <div className={css.taskNameWrapper}>
+            <div className={css.taskTitle}>
+              <div className={css.meta}>
+                <span>QiwA-3214</span>
+                <span>Киви-Банк - Артек</span>
+                <span>QA</span>
+              </div>
+              <div className={css.taskName}>
+                Игровой клиент: таблица результатов
+              </div>
+            </div>
+          </div>
+          <div className={css.time}>
+            <div className={css.today}>
+              <input key="item3" type="text" onChange={(e) => this.handleChangeItem('item3', e)} value={this.state.times.item3}/>
+            </div>
+            <div className={css.other}>
+              <span data-tip="Потрачено" data-place="bottom">0</span> / <span data-tip="Запланировано" data-place="bottom">0</span>
+            </div>
+          </div>
+        </div>
+        <div className={classnames(css.listTask, css.task)}>
+          <div className={classnames(css.actionButton, css.locked)}>
+            <IconLock style={{width: '1.5rem', height: '1.5rem'}} />
+          </div>
+          <div className={css.taskNameWrapper}>
+            <div className={css.taskTitle}>
+              <div className={css.meta}>
+                <span>ST-3214</span>
+                <span>SimTrack</span>
+                <span>QA</span>
+              </div>
+              <div className={css.taskName}>
+                При переводе между членами семьи в списке отображаться роль
+              </div>
+            </div>
+          </div>
+          <div className={css.time}>
+            <div className={css.today}>
+              <input key="item3" type="text" onChange={(e) => this.handleChangeItem('item3', e)} value={this.state.times.item3}/>
+            </div>
+            <div className={css.other}>
+              <span data-tip="Потрачено" data-place="bottom">0</span> / <span data-tip="Запланировано" data-place="bottom">0</span>
             </div>
           </div>
         </div>
@@ -128,7 +188,7 @@ class Playlist extends Component {
           </div>
           <div className={css.taskNameWrapper}>
             <div className={css.taskTitle}>
-              <div className={css.taskSubtitle}>
+              <div className={css.meta}>
                 Активная задача: ST-48
               </div>
               <div className={css.taskName}>
@@ -142,15 +202,58 @@ class Playlist extends Component {
             isPlaylistOpen
             ? <div className={css.list}>
                 <div className={css.week}>
-                  <div className={css.day}>Пн</div>
-                  <div className={classnames(css.day, css.active)}>Вт</div>
+                  <div className={css.day}>Пн <span className={css.countBadge}>1</span></div>
+                  <div className={classnames(css.day, css.active)}>Вт <span className={css.countBadge}>65</span></div>
                   <div className={css.day}>Ср</div>
                   <div className={css.day}>Чт</div>
                   <div className={css.day}>Пт</div>
                   <div className={css.day}>Сб</div>
                   <div className={css.day}>Вс</div>
                 </div>
-                {tasks}
+                <div className={css.taskWrapper}>
+                  {tasks}
+                  {
+                    isNotMineTasksShow
+                    ? notMineTasks
+                    : null
+                  }
+                  <div
+                    className={css.showMore}
+                    onClick={this.handleShowOther}
+                    data-tip={!isNotMineTasksShow ? 'Показать переведенные' : 'Скрыть переведенные'}
+                    data-place="bottom">
+                    {
+                      !isNotMineTasksShow
+                      ? <IconArrowDown/>
+                      : <IconArrowUp/>
+                    }
+                  </div>
+                </div>
+                <div className={css.activity}>
+                  <div className={classnames(css.type, css.active)} data-tip="Все активности" data-place="bottom">
+                    <IconList/>
+                  </div>
+                  <div className={css.type} data-tip="Работа" data-place="bottom">
+                    <IconLaptop/>
+                  </div>
+                  <div className={css.type} data-tip="Совещание" data-place="bottom">
+                    <IconCall/>
+                  </div>
+                  <div className={css.type} data-tip="Оценка" data-place="bottom">
+                    <IconTime/>
+                  </div>
+                  <div className={css.type} data-tip="Обучение" data-place="bottom">
+                    <IconBook/>
+                  </div>
+                  <div className={css.type} data-tip="Отпуск" data-place="bottom">
+                    <IconPlane/>
+                  </div>
+                  <div className={css.time}>
+                    <div className={css.today}>
+                      <input type="text" value={7.25} data-tip="Итого"/>
+                    </div>
+                  </div>
+                </div>
             </div>
             : null
           }
