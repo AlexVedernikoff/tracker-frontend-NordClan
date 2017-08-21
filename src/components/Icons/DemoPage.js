d4d81845d1ba3b701c2e3f1f106d54dfa1713d6a
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-08-21 14:19:16 +0400

    Progress circle

--- src/components/Icons/DemoPage.js
+++ src/components/Icons/DemoPage.js
@@ -8,7 +8,6 @@ export default class DemoPage extends Component {
     for (const element in allIcons) {
       iconsList.push(<div key={element} className={css.demoIcon}>{allIcons[element]()}<div>{element}</div></div>);
     }
-    console.log(allIcons);
     return (
       <section className={css.demoContainer}>
         {iconsList}
