62440687a3ed9b028c7e43da0c92b8d3ccedff9c
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-09-06 10:45:50 +0400

    Правка url для обновления тш

--- src/actions/TimesheetPlayer.js
+++ src/actions/TimesheetPlayer.js
@@ -79,7 +79,7 @@ export const updateExistedTimesheet = (data, options) => {
 
   return dispatch => dispatch({
     type: REST_API,
-    url: `/task/${data.taskId}/timesheet/${data.timesheetId}`,
+    url: `/timesheet/${data.timesheetId}`,
     method: PUT,
     body: data.body,
     extra,
