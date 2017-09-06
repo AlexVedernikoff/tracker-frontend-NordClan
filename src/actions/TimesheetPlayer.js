6c30e723e4f590b349669cc0ec3ccfc47bfe0447
Владимир Чапурин <vladimir.chapurin@simbirsoft.com>
2017-09-06 11:47:27 +0400

    Добавил isDraft: false в роут обновления существующего тш

--- src/actions/TimesheetPlayer.js
+++ src/actions/TimesheetPlayer.js
@@ -81,7 +81,7 @@ export const updateExistedTimesheet = (data, options) => {
     type: REST_API,
     url: `/timesheet/${data.timesheetId}`,
     method: PUT,
-    body: data.body,
+    body: {isDraft: false, ...data.body},
     extra,
     start: withStartLoading(startReceivePlayerData, true)(dispatch),
     response: withFinishLoading(response => {
