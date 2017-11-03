2916fb0f2de1671b77906fde0ecf0338f47431aa
Irina Bilalova <irina.bilalova@simbirsoft.com>
2017-11-03 14:15:43 +0400

    added project icon

---
+++ src/components/ProjectIcon/ProjectIcon.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as css from './ProjectIcon.scss';
+
+const ProjectIcon = props => {
+  const name = props.projectName;
+
+  const hexToLuma = hex => {
+    const r = parseInt(hex.substr(0, 2), 16);
+    const g = parseInt(hex.substr(2, 2), 16);
+    const b = parseInt(hex.substr(4, 2), 16);
+    return [0.299 * r, 0.587 * g, 0.114 * b].reduce((a, с) => a + с) / 255;
+  };
+
+  const backColor = parseInt(
+    btoa(encodeURI(name))
+      .toLowerCase()
+      .replace(/[^a-z0-9]/, ''),
+    36
+  )
+    .toString(16)
+    .substr(0, 6);
+
+  const fontColor = hexToLuma(backColor) > 0.5 ? '#333' : '#fafafa';
+
+  const divStyle = {
+    backgroundColor: `#${backColor}`,
+    color: fontColor
+  };
+
+  return (
+    <div className={css.projectIcon} style={divStyle}>
+      <span>
+        {name
+          .split(' ')
+          .map(item => item[0])
+          .join('')
+          .substr(0, 2)
+          .toUpperCase()}
+      </span>
+    </div>
+  );
+};
+
+ProjectIcon.propTypes = {
+  projectName: PropTypes.string
+};
+
+export default ProjectIcon;
