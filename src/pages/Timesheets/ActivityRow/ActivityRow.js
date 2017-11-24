edc000fd870fe75a0df5a8cabfade11aab167c26
Andrew Yudin <andrew.yudin@simbirsoft.com>
2017-11-24 17:42:18 +0400

    ST-457 disable submitted timesheets

--- src/pages/Timesheets/ActivityRow/ActivityRow.js
+++ src/pages/Timesheets/ActivityRow/ActivityRow.js
@@ -153,6 +153,7 @@ class ActivityRow extends React.Component {
               })}>
                 <input
                   type="number"
+                  disabled={tsh.id === 3}
                   max="24"
                   defaultValue={roundNum(tsh.spentTime, 2)}
                   onChange={(e) => this.changeFilled(i, tsh.id, tsh.comment, e)}
