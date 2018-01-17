d392847bde4d8f7087a5111d59e4eeebae9c1924
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-01-17 18:02:15 +0400

    add actions for getting all users

---
+++ src/actions/users.js
@@ -0,0 +1,35 @@
+import * as UsersActions from '../constants/Users';
+import axios from 'axios';
+import { API_URL } from '../constants/Settings';
+import { startLoading, finishLoading } from './Loading';
+import { showNotification } from './Notifications';
+
+const startUsersReceive = () => ({
+  type: UsersActions.USERS_RECEIVE_START
+});
+
+const UsersReceived = users => ({
+  type: UsersActions.USERS_RECEIVE_SUCCESS,
+  data: users
+});
+
+const getUsers = () => {
+  const URL = `${API_URL}/user/all`;
+  return dispatch => {
+    dispatch(startUsersReceive());
+    dispatch(startLoading());
+    axios.get(URL, {}, { withCredentials: true })
+      .catch(error => {
+        dispatch(finishLoading());
+        dispatch(showNotification({ message: error.message, type: 'error' }));
+      })
+      .then(response => {
+        if (response && response.status === 200) {
+          dispatch(UsersReceived(response.data));
+          dispatch(finishLoading());
+        }
+      });
+  };
+};
+
+export default getUsers;
