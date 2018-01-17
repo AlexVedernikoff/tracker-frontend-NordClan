9f757385c31da13c7848484c9ff7136d0661f80d
Olesia <olesiatenkova@gmail.com>
2018-01-17 10:29:47 +0400

    ST-895 show login error

--- src/actions/Authentication.js
+++ src/actions/Authentication.js
@@ -6,6 +6,7 @@ import { showNotification } from './Notifications';
 import { getTimesheetsPlayerData } from './TimesheetPlayer';
 import { startOfCurrentWeek, endOfCurrentWeek } from '../utils/date';
 import { history } from '../History';
+import { getErrorMessageByType } from '../utils/ErrorMessages';
 
 const startAuthentication = () => ({
   type: AuthActions.AUTHENTICATION_START
@@ -60,7 +61,7 @@ export const doAuthentication = ({ username, password }) => {
       )
       .catch(error => {
         if (error.response.data.name === 'InvalidCredentialsError' && error.response.data.code === 49) {
-          dispatch(authenticationError('Неверый логин/пароль. Проверьте данные'));
+          dispatch(authenticationError(getErrorMessageByType(error.response.data.name)));
         } else {
           dispatch(showNotification({message: error.message, type: 'error'}));
         }
