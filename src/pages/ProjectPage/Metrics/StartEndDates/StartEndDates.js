93a452d53c6795d847e68587d9f225a212b548d3
dmitry.pigalov <dmitry.pigalov@simbirsoft.com>
2018-01-18 19:30:26 +0400

    ST-488 refact startEndDates

--- src/pages/ProjectPage/Metrics/StartEndDates/StartEndDates.js
+++ src/pages/ProjectPage/Metrics/StartEndDates/StartEndDates.js
@@ -6,26 +6,29 @@ import moment from 'moment';
 
 class StartEndDates extends Component {
   constructor (props) {
-    super(props)
+    super(props);
   }
   render () {
     return (
-      <div className = {css.startEndDatesWrp}> 
+      <div className = {css.startEndDatesWrp}>
         <div className = {css.startEndDates}>
           Дата начала:
-          <Input readOnly value = {this.props.startDate ? moment(this.props.startDate).format('YYYY-MM-DD') : 'Загрузка...'}/>
+          <Input
+            readOnly
+            value={this.props.startDate ? moment(this.props.startDate).format('YYYY-MM-DD') : ''}/>
         </div>
         <div className = {css.startEndDates}>
           Дата завершения:
-          <Input readOnly value = {this.props.endDate ? moment(this.props.endDate).format('YYYY-MM-DD') : 'Загрузка...'}/>
+          <Input
+            readOnly
+            value={this.props.endDate ? moment(this.props.endDate).format('YYYY-MM-DD') : ''}/>
         </div>
       </div>
-      
-    )
+    );
   }
 }
 StartEndDates.propTypes = {
-  createdAt: PropTypes.string,
-  completedAt: PropTypes.string
+  startDate: PropTypes.string,
+  endDate: PropTypes.string
 };
 export default StartEndDates;
