e5a434a6ad11ece031dba663792d292f62b4095a
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-01-23 13:22:24 +0400

    improve display statuses the current task and the create draft

--- src/pages/InnerContainer/AppHead/Playlist/PlaylistItem/PlaylistItem.js
+++ src/pages/InnerContainer/AppHead/Playlist/PlaylistItem/PlaylistItem.js
@@ -104,13 +104,14 @@ class PlaylistItem extends Component {
   };
 
   render () {
+    console.log(this.props);
     const {
       task,
       project,
       spentTime,
       comment,
       typeId,
-      taskStatus: prevStatus,
+      taskStatus: createDraftStatus,
       isDraft,
       isVisible
     } = this.props.item;
@@ -121,6 +122,8 @@ class PlaylistItem extends Component {
 
     const prefix = project ? project.prefix : '';
     const taskLabel = task && project ? `${project.prefix}-${task.id}` : null;
+
+    const createDraftStatusName = createDraftStatus ? createDraftStatus.name.replace(' stop', '') : '';
 
     return (
       <div className={classnames(css.listTask, css.task)}>
@@ -138,13 +141,17 @@ class PlaylistItem extends Component {
               { status
                 ? <span>
                   {
-                    prevStatus
-                      ? (<span>{prevStatus.name}<span style={{display: 'inline-block', margin: '0 0.25rem'}}> → </span></span>)
+                    createDraftStatus
+                      ? (<span>{createDraftStatusName}<span /></span>)
                       : null
                   }
-                  {status.name}
                 </span>
                 : null}
+                { status
+                  ? <span>
+                    Тек. статус: {status.name}
+                  </span>
+                  : null}
               {
                 !isDraft
                   ? <span className={classnames({[css.commentToggler]: true, [css.green]: !!comment})} onClick={this.toggleComment}><IconComment/></span>
