b364910704ebec2c957d577861cfb4e51c8ba721
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-02-08 12:22:23 +0400

    improve users roles reducer

--- src/reducers/UsersRoles.js
+++ src/reducers/UsersRoles.js
@@ -8,18 +8,14 @@ const InitialState = {
 function Users (state = InitialState, action) {
   switch (action.type) {
   case UsersRolesActions.GET_USERS_START:
-    return {
-      ...state
-    };
+    return state;
   case UsersRolesActions.GET_USERS_SUCCESS:
     return {
       ...state,
       users: action.data
     };
   case UsersRolesActions.CHANGE_USER_STATUS_START:
-    return {
-      ...state
-    };
+    return state;
   case UsersRolesActions.CHANGE_USER_STATUS_SUCCESS:
     const userId = action.user.id;
     const userIndex = _.findIndex(state, { id: userId });
