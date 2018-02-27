9386e590ae36701d3f1f74eab04fd6edcacc69cb
Ivan <inferal73@gmail.com>
2018-02-27 11:38:26 +0400

    ST-1361 adds empty properties to input and hides the search field in user roles dropdown select

--- src/components/SelectDropdown/SelectDropdown.js
+++ src/components/SelectDropdown/SelectDropdown.js
@@ -5,11 +5,24 @@ import PropTypes from 'prop-types';
 
 export default class SelectDropdown extends Component {
   static propTypes = {
+    inputCanBeEmpty: PropTypes.bool,
     name: PropTypes.string,
     options: PropTypes.array
   };
 
-  render () {
+  static defaultProps = {
+    inputCanBeEmpty: true
+  };
+
+  onInputKeyDown(event) {
+    const isEmpty = event.target.value ? event.target.value.length === 0 : true;
+    if (!this.props.inputCanBeEmpty && event.keyCode === 8 && isEmpty) {
+      event.preventDefault();
+      return false;
+    }
+  }
+
+  render() {
     const { name, options, thisClassName, noResultsText, ...other } = this.props;
 
     return (
@@ -18,7 +31,8 @@ export default class SelectDropdown extends Component {
         name={name}
         options={options}
         noResultsText="Ничего не найдено"
-        onFocus={(e) => e.stopPropagation()}
+        onInputKeyDown={e => this.onInputKeyDown(e)}
+        onFocus={e => e.stopPropagation()}
         {...other}
       />
     );
