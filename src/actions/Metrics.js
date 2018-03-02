35c108a34d670f75ba7af5f1f901024f30e96d73
Aleksandr Zotov <aleksandr.zotov@simbirsoft.com>
2018-03-02 12:55:40 +0400

    change method for getting metrics on the POST

--- src/actions/Metrics.js
+++ src/actions/Metrics.js
@@ -2,17 +2,13 @@ import axios from 'axios';
 import * as ProjectActions from '../constants/Project';
 import { GET, POST, REST_API } from '../constants/RestApi';
 import { API_URL } from '../constants/Settings';
-import {
-  withFinishLoading,
-  withStartLoading,
-  defaultExtra as extra
-} from './Common';
+import { withFinishLoading, withStartLoading, defaultExtra as extra } from './Common';
 
 const getMetricsRequest = () => ({
   type: ProjectActions.GET_METRICS_REQUEST
 });
 
-const getMetricsSuccess = (metrics) => ({
+const getMetricsSuccess = metrics => ({
   type: ProjectActions.GET_METRICS_SUCCESS,
   metrics
 });
@@ -21,21 +17,22 @@ const getMetricsFailure = () => ({
   type: ProjectActions.GET_METRICS_FAILURE
 });
 
-export const getMetrics = (data) => {
-  return dispatch => dispatch({
-    type: REST_API,
-    url: '/metrics',
-    method: GET,
-    body: {
-      params: { ...data }
-    },
-    extra,
-    start: withStartLoading(getMetricsRequest, true)(dispatch),
-    response: withFinishLoading(response => {
-      dispatch(getMetricsSuccess(response.data));
-    })(dispatch),
-    error: withFinishLoading(() => {
-      dispatch(getMetricsFailure(dispatch));
-    })(dispatch)
-  });
+export const getMetrics = data => {
+  return dispatch =>
+    dispatch({
+      type: REST_API,
+      url: '/metrics',
+      method: POST,
+      body: {
+        ...data
+      },
+      extra,
+      start: withStartLoading(getMetricsRequest, true)(dispatch),
+      response: withFinishLoading(response => {
+        dispatch(getMetricsSuccess(response.data));
+      })(dispatch),
+      error: withFinishLoading(() => {
+        dispatch(getMetricsFailure(dispatch));
+      })(dispatch)
+    });
 };
