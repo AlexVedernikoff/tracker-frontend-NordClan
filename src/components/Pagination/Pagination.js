bc1f3fdb895711afc942f6d2de597c5de7031b35
evgeniy petrov <evgeniy.petrov@simbirsoft.com>
2018-05-04 12:31:51 +0400

    ST-1713 Added a "disabled" style to the active pagination page and removed the event by clicking on the active page

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import * as css from './Pagination.scss';
 
-const Pagination = (props) => {
-  const { itemsCount, className, activePage, onItemClick, prevText, nextText } = props;
+const Pagination = props => {
+  const { itemsCount, activePage, onItemClick, prevText, nextText } = props;
 
   const PrevDisabled = activePage === 1;
   const NextDisabled = activePage === itemsCount;
@@ -43,31 +43,49 @@ const Pagination = (props) => {
   const items = [];
   for (let i = 0; i < itemsCount; i++) {
     const index = i + 1;
+    const active = activePage === index;
     items.push(
-      <li key={i}
-          className={classnames({
-            [css['page-item']]: true,
-            [css.active]: activePage === index
-          })}
-          onClick={(e) => handleClick({ activePage: index }, e)}
+      <li
+        key={i}
+        className={classnames({
+          [css['page-item']]: true,
+          [css.active]: active,
+          [css.disabled]: active
+        })}
+        onClick={e => !active && handleClick({ activePage: index }, e)}
       >
-        <a className={css['page-link']} href="#">{index}</a>
-      </li>);
+        <a className={css['page-link']} href="#">
+          {index}
+        </a>
+      </li>
+    );
   }
 
   return (
     <ul className={css.pagination}>
-      <li className={classnames({
-        [css['page-item']]: true,
-        [css.disabled]: activePage === 1
-      })} onClick={(e) => handleClick({ isPrev: true }, e)}><a className={css['page-link']} href="#">{prevText || '«'}</a></li>
-      {
-        itemsCount && items
-      }
-      <li className={classnames({
-        [css['page-item']]: true,
-        [css.disabled]: activePage === itemsCount
-      })} onClick={(e) => handleClick({ isNext: true }, e)}><a className={css['page-link']} href="#">{nextText || '»'}</a></li>
+      <li
+        className={classnames({
+          [css['page-item']]: true,
+          [css.disabled]: activePage === 1
+        })}
+        onClick={e => handleClick({ isPrev: true }, e)}
+      >
+        <a className={css['page-link']} href="#">
+          {prevText || '«'}
+        </a>
+      </li>
+      {itemsCount && items}
+      <li
+        className={classnames({
+          [css['page-item']]: true,
+          [css.disabled]: activePage === itemsCount
+        })}
+        onClick={e => handleClick({ isNext: true }, e)}
+      >
+        <a className={css['page-link']} href="#">
+          {nextText || '»'}
+        </a>
+      </li>
     </ul>
   );
 };
