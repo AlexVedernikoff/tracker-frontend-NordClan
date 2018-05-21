4dba5a659755355daccfadda3ee7df0c53f07b87
Andrew Yudin <andrew.yudin@simbirsoft.com>
2018-05-21 18:24:18 +0400

    ST-2342: gitlab component

---
+++ src/pages/ProjectPage/Settings/GitLabEditor/GitLabEditor.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import * as css from './GitLabEditor.scss';
+import Input from '../../../../components/Input';
+import RoundButton from '../../../../components/RoundButton';
+import { IconCheck } from '../../../../components/Icons';
+import { changeProject } from '../../../../actions/Project';
+
+class GitLabEditor extends Component {
+  static propTypes = {
+    changeProject: PropTypes.func,
+    project: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
+  changeValue = e => {
+    const value = e.target.value;
+    this.setState({ value });
+  };
+
+  submit = () => {
+    this.props.changeProject(
+      {
+        gitlabRepositoryId: this.state.value,
+        id: this.props.project.id
+      },
+      'gitlabRepositoryId'
+    );
+  };
+
+  render() {
+    const { value } = this.state;
+    const onChange = this.changeValue;
+
+    return (
+      <div className={css.gitLabEditor}>
+        <h2>GitLab</h2>
+        <Input {...{ value, onChange }} />
+        <RoundButton
+          data-tip="Сохранить"
+          // loading={!!loading}
+          onClick={this.submit}
+          // disabled={this.isMoveTasksButtonDisabled(unfinishedLeftTasksCount)}
+        >
+          <IconCheck />
+        </RoundButton>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    project: state.Project.project
+  };
+}
+
+const mapDispatchToProps = {
+  changeProject
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GitLabEditor);
