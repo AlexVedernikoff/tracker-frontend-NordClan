ce1184d8f91e10f8e783e89946e67838af3ddf4b
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2018-06-07 12:07:25 +0400

    Add select вех

--- src/pages/ProjectPage/Planning/CreateMilestoneModal/CreateMilestoneModal.js
+++ src/pages/ProjectPage/Planning/CreateMilestoneModal/CreateMilestoneModal.js
@@ -9,6 +9,26 @@ import * as css from './CreateMilestoneModal.scss';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { createMilestone } from '../../../../actions/Milestone';
+import Select from 'react-select';
+
+const options = [
+  {
+    label: 'Получение отзыва',
+    value: 1
+  },
+  {
+    label: 'Демо Клиенту',
+    value: 2
+  },
+  {
+    label: 'Внутренняя демо',
+    value: 3
+  },
+  {
+    label: 'Другое',
+    value: 4
+  }
+];
 
 class CreateMilestoneModal extends Component {
   static propTypes = {
@@ -17,12 +37,13 @@ class CreateMilestoneModal extends Component {
     projectId: PropTypes.number
   };
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       date: undefined,
-      name: ''
+      name: '',
+      enum: 1
     };
   }
 
@@ -34,32 +55,30 @@ class CreateMilestoneModal extends Component {
     this.setState({ date: date ? moment(date).format('YYYY-MM-DD') : '' });
   };
 
+  changeStatus = status => {
+    this.setState({ enum: status.value });
+  };
+
   checkNullInputs = () => {
     return this.state.name.trim() && this.state.date;
   };
 
-  dateInputHandler = (e) => {
+  dateInputHandler = e => {
     const inputValue = e.target.value;
     const isValidValue = moment(inputValue, 'DD.MM.YYYY', true).isValid();
     if (!isValidValue && this.state.date) {
-      this.setState({date: ''});
+      this.setState({ date: '' });
     }
-  }
+  };
 
   createMilestone = e => {
     e.preventDefault();
     this.props.onClose();
-    this.props.createMilestone(
-      this.state.name.trim(),
-      this.props.projectId,
-      this.state.date,
-    );
+    this.props.createMilestone(this.state.name.trim(), this.props.projectId, this.state.date);
   };
 
-  render () {
-    const formattedDay = this.state.date
-      ? moment(this.state.date).format('DD.MM.YYYY')
-      : '';
+  render() {
+    const formattedDay = this.state.date ? moment(this.state.date).format('DD.MM.YYYY') : '';
 
     const formLayout = {
       firstCol: 4,
@@ -78,13 +97,7 @@ class CreateMilestoneModal extends Component {
             </Row>
             <Row>
               <Col xs={12} className={css.validateMessages}>
-                {
-                  !this.checkNullInputs()
-                    ? <span>
-                      Все поля должны быть заполнены
-                    </span>
-                    : null
-                }
+                {!this.checkNullInputs() ? <span>Все поля должны быть заполнены</span> : null}
               </Col>
             </Row>
             <Row className={css.inputRow}>
@@ -92,9 +105,23 @@ class CreateMilestoneModal extends Component {
                 <p>Название вехи:</p>
               </Col>
               <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
-                <Input
-                  placeholder="Введите название вехи"
-                  onChange={this.onChangeName}
+                <Input placeholder="Введите название вехи" onChange={this.onChangeName} />
+              </Col>
+            </Row>
+            <Row className={css.inputRow}>
+              <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
+                <p>Типы майлстоунов:</p>
+              </Col>
+              <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
+                <Select
+                  value={this.state.enum}
+                  options={options}
+                  multi={false}
+                  style={{ width: '100%' }}
+                  className={css.selectEnum}
+                  onChange={this.changeStatus}
+                  placeholder="Типы майлстоунов"
+                  noResultsText="Нет результатов"
                 />
               </Col>
             </Row>
--- src/pages/ProjectPage/Planning/EditMilestoneModal/EditMilestoneModal.js
+++ src/pages/ProjectPage/Planning/EditMilestoneModal/EditMilestoneModal.js
@@ -11,9 +11,29 @@ import Input from '../../../../components/Input';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { editMilestone } from '../../../../actions/Milestone';
+import Select from 'react-select';
+
+const options = [
+  {
+    label: 'Получение отзыва',
+    value: 1
+  },
+  {
+    label: 'Демо Клиенту',
+    value: 2
+  },
+  {
+    label: 'Внутренняя демо',
+    value: 3
+  },
+  {
+    label: 'Другое',
+    value: 4
+  }
+];
 
 class EditMilestoneModal extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -21,7 +41,8 @@ class EditMilestoneModal extends Component {
         id: this.props.milestone.id,
         name: this.props.milestone.name,
         date: this.props.milestone.date,
-        done: this.props.milestone.done
+        done: this.props.milestone.done,
+        enum: 1
       }
     };
   }
@@ -46,6 +67,15 @@ class EditMilestoneModal extends Component {
     }));
   };
 
+  changeStatus = status => {
+    this.setState(state => ({
+      milestone: {
+        ...state.milestone,
+        enum: status.value
+      }
+    }));
+  };
+
   handleStatusChange = e => {
     e.persist();
     this.setState(state => ({
@@ -56,7 +86,7 @@ class EditMilestoneModal extends Component {
     }));
   };
 
-  handleEditMilestone = (e) => {
+  handleEditMilestone = e => {
     e.preventDefault();
     this.props.onClose();
     this.props.editMilestone({
@@ -69,10 +99,8 @@ class EditMilestoneModal extends Component {
     return this.state.milestone.name.trim() && this.state.milestone.date;
   };
 
-  render () {
-    const formattedDay = this.state.milestone.date
-      ? moment(this.state.milestone.date).format('DD.MM.YYYY')
-      : '';
+  render() {
+    const formattedDay = this.state.milestone.date ? moment(this.state.milestone.date).format('DD.MM.YYYY') : '';
 
     const formLayout = {
       firstCol: 5,
@@ -80,11 +108,7 @@ class EditMilestoneModal extends Component {
     };
 
     return (
-      <Modal
-        isOpen
-        contentLabel="modal"
-        onRequestClose={this.props.onClose}
-      >
+      <Modal isOpen contentLabel="modal" onRequestClose={this.props.onClose}>
         <div>
           <form className={css.createSprintForm}>
             <Row className={css.inputRow}>
@@ -95,32 +119,36 @@ class EditMilestoneModal extends Component {
             </Row>
             <Row>
               <Col xs={12} className={css.validateMessages}>
-                {
-                  !this.checkNullInputs()
-                    ? <span>
-                      Все поля должны быть заполнены
-                    </span>
-                    : null
-                }
+                {!this.checkNullInputs() ? <span>Все поля должны быть заполнены</span> : null}
               </Col>
             </Row>
             <Row className={css.inputRow}>
-              <Col
-                xs={12}
-                sm={formLayout.firstCol}
-                className={css.leftColumn}
-              >
+              <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
                 <p>Название вехи:</p>
               </Col>
-              <Col
-                xs={12}
-                sm={formLayout.secondCol}
-                className={css.rightColumn}
-              >
+              <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
                 <Input
                   placeholder="Введите название вехи"
                   defaultValue={this.state.milestone.name}
                   onChange={this.onChangeName}
+                />
+              </Col>
+            </Row>
+
+            <Row className={css.inputRow}>
+              <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
+                <p>Типы майлстоунов:</p>
+              </Col>
+              <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
+                <Select
+                  value={this.state.milestone.enum}
+                  options={options}
+                  multi={false}
+                  style={{ width: '100%' }}
+                  className={css.selectEnum}
+                  onChange={this.changeStatus}
+                  placeholder="Типы майлстоунов"
+                  noResultsText="Нет результатов"
                 />
               </Col>
             </Row>
@@ -144,10 +172,7 @@ class EditMilestoneModal extends Component {
                 <p>Выполнено</p>
               </Col>
               <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
-                <Checkbox
-                  checked={this.state.milestone.done}
-                  onChange={this.handleStatusChange}
-                />
+                <Checkbox checked={this.state.milestone.done} onChange={this.handleStatusChange} />
               </Col>
             </Row>
 
@@ -170,7 +195,7 @@ class EditMilestoneModal extends Component {
 }
 
 const mapStateToProps = state => ({
-  projectId: state.Project.project.id,
+  projectId: state.Project.project.id
 });
 
 const mapDispatchToProps = {
