51cb29602051bba299c86596fc8fcd43edbb10ab
Marat <alembikovm@gmail.com>
2018-08-01 00:30:06 +0400

    ST-5182 Edited name service

--- src/utils/NameLocalisation.js
+++ src/utils/NameLocalisation.js
@@ -13,22 +13,28 @@ export function getLastName(user) {
   return lang === 'ru' ? user.lastNameRu : user.lastNameEn;
 }
 
-export function getFullName(user) {
-  if (lang === 'ru') {
-    if (user.fullNameRu) {
-      return user.fullNameRu;
-    } else if (!user.fullNameRu && user.firstNameRu && user.lastNameRu) {
-      return `${user.firstNameRu} ${user.lastNameRu}`;
-    } else {
-      return user.fullNameEn || `${user.firstNameEn} ${user.lastNameEn}`;
-    }
-  } else if (lang === 'en') {
-    if (user.fullNameEn) {
-      return user.fullNameEn;
-    } else if (!user.fullNameEn && user.firstNameEn && user.lastNameEn) {
-      return `${user.firstNameEn} ${user.lastNameEn}`;
-    } else {
-      return user.fullNameRu || `${user.firstNameRu} ${user.lastNameRu}`;
-    }
+const fullEn = 'fullNameEn';
+const fullRu = 'fullNameRu';
+const firstEn = 'firstNameEn';
+const firstRu = 'firstNameRu';
+const lastEn = 'lastNameEn';
+const lastRu = 'lastNameRu';
+
+const config = {
+  en: { full: fullEn, altFull: fullRu, first: firstEn, last: lastEn },
+  ru: { full: fullRu, altFull: fullEn, first: firstRu, last: lastRu }
+};
+
+const getLocalize = ({ full, first, last, altFull }, user) => {
+  if (user[full]) {
+    return user[full];
+  } else if (user[first] && user[last]) {
+    return `${user[first]} ${user[last]}`;
+  } else {
+    return user[altFull];
   }
+};
+
+export function getFullName(user) {
+  return getLocalize(config[lang], user);
 }
