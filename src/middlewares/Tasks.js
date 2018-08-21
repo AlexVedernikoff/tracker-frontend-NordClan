fed4366c0b8984303d79eb7dd461b179ed03e59a
petyankin.av <aleksandr.petyankin@simbirsoft.com>
2018-08-21 18:04:06 +0300

    ST-7078 Fix pageSize property

--- src/middlewares/Tasks.js
+++ src/middlewares/Tasks.js
@@ -1,8 +1,4 @@
-import {
-    TASK_CHANGE_REQUEST_SUCCESS,
-    TASK_CHANGE_USER_SUCCESS,
-    TASK_LINK_SUCCESS
-} from '../constants/Task';
+import { TASK_CHANGE_REQUEST_SUCCESS, TASK_CHANGE_USER_SUCCESS, TASK_LINK_SUCCESS } from '../constants/Task';
 import { getTaskHistory } from '../actions/Task';
 
 const targetActions = {
@@ -11,11 +7,13 @@ const targetActions = {
   [TASK_LINK_SUCCESS]: true
 };
 
-export const taskUpdate = (store) => (next) => (action) => {
+export const taskUpdate = store => next => action => {
   if (targetActions[action.type]) {
-    const {routing: { locationBeforeTransitions: location }, Task: { task: { id } } } = store.getState();
+    const { routing: { locationBeforeTransitions: location }, Task: { task: { id } } } = store.getState();
     if (RegExp(`${id}\/history\/?$`).test(location.pathname)) {
-      store.dispatch(getTaskHistory(id));
+      store.getState().Task.history && store.getState().Task.history.pageSize
+        ? store.dispatch(getTaskHistory(id, { pageSize: store.getState().Task.history.pageSize }))
+        : store.dispatch(getTaskHistory(id));
     }
   }
   next(action);
