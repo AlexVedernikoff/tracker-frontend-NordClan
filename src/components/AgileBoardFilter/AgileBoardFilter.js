01a8e7e284b96e14680661e9e69f294d909d9a30
Anton Bistryakov <anton.bistryakov@simbirsoft.com>
2018-09-28 13:59:18 +0400

    ST-9105 Edited condition

--- src/components/AgileBoardFilter/AgileBoardFilter.js
+++ src/components/AgileBoardFilter/AgileBoardFilter.js
@@ -38,10 +38,9 @@ class AgileBoardFilter extends React.Component {
       prevProps.project.users !== this.props.project.users ||
       prevProps.typeOptions !== this.props.typeOptions ||
       (!this.state.allFilters.length && !this.props.isFilterEmpty) ||
-      (this.state.allFilters.length &&
-        this.state.allFilters[0].label === 'Backlog' &&
-        this.props.filters.changedSprint[0] !== 0) ||
-      (this.state.allFilters.length === 1 && boolFilters.indexOf(true) !== boolFilters.lastIndexOf(true))
+      (this.state.allFilters.length =
+        1 && this.state.allFilters[0].label === 'Backlog' && this.props.filters.changedSprint[0] !== 0) ||
+      (this.state.allFilters.length === 1 && boolFilters.filter(f => f).length > 1)
     ) {
       this.updateFilterList();
     }
