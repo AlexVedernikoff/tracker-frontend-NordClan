697483a68f3c9f2ac8c8bd30607d04b6138cb684
Marat Alembikov <marat.alembikov@simbirsoft.com>
2018-10-03 11:32:34 +0400

    ST-7196 recovered mainSrc, edited conditionals for prevSrc and nextSrc

--- src/components/Attachments/Attachments.js
+++ src/components/Attachments/Attachments.js
@@ -114,14 +114,13 @@ export default class Attachments extends Component {
     let mainSrc = '';
 
     if (attachments.length) {
-      mainSrc = `/${attachments[photoIndex].path}`;
+      mainSrc = attachments[photoIndex].path;
       nextSrc =
-        attachments[nextImageIndex((photoIndex + 1) % attachments.length)].path !== mainSrc && attachments.length > 1
+        attachments[nextImageIndex((photoIndex + 1) % attachments.length)].path !== mainSrc
           ? attachments[nextImageIndex((photoIndex + 1) % attachments.length)].path
           : undefined;
       prevSrc =
-        attachments[prevImageIndex((photoIndex + attachments.length - 1) % attachments.length)].path !== mainSrc &&
-        attachments.length > 1
+        attachments[prevImageIndex((photoIndex + attachments.length - 1) % attachments.length)].path !== mainSrc
           ? attachments[prevImageIndex((photoIndex + attachments.length - 1) % attachments.length)].path
           : undefined;
     }
@@ -134,6 +133,7 @@ export default class Attachments extends Component {
         </ul>
         {isOpen && (
           <Lightbox
+            mainSrc={`/${mainSrc}`}
             nextSrc={nextSrc}
             prevSrc={prevSrc}
             onCloseRequest={this.closeImage}
