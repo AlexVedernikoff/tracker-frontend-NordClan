0e5dc779baadbd9adbe6ff170bf01ebd5f6cb396
Anton Bistryakov <anton.bistryakov@simbirsoft.com>
2018-10-16 10:31:45 +0400

    ST-9992 Removed changes from ST-8000

--- src/pages/ProjectPage/ProjectTimesheets/EditSpentModal/EditSpentModal.js
+++ src/pages/ProjectPage/ProjectTimesheets/EditSpentModal/EditSpentModal.js
@@ -8,14 +8,6 @@ import Checkbox from '../../../../components/Checkbox';
 import SelectDropdown from '../../../../components/SelectDropdown';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
 import * as css from './EditSpentModal.scss';
-import {
-  TASK_STATUS_CODE_REVIEW_PLAY,
-  TASK_STATUS_CODE_REVIEW_STOP,
-  TASK_STATUS_DEVELOP_PLAY,
-  TASK_STATUS_DEVELOP_STOP,
-  TASK_STATUS_QA_PLAY,
-  TASK_STATUS_QA_STOP
-} from '../../../../constants/Task';
 
 class EditSpentModal extends Component {
   static propTypes = {
@@ -104,24 +96,6 @@ class EditSpentModal extends Component {
       secondCol: 8
     };
 
-    const billableRow = () => {
-      return taskStatusId !== TASK_STATUS_DEVELOP_PLAY &&
-        taskStatusId !== TASK_STATUS_DEVELOP_STOP &&
-        taskStatusId !== TASK_STATUS_CODE_REVIEW_PLAY &&
-        taskStatusId !== TASK_STATUS_CODE_REVIEW_STOP &&
-        taskStatusId !== TASK_STATUS_QA_STOP &&
-        taskStatusId !== TASK_STATUS_QA_PLAY ? (
-        <Row className={css.inputRow}>
-          <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
-            <p>Billable:</p>
-          </Col>
-          <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
-            <Checkbox disabled checked={isBillable} onChange={this.changeBillable} />
-          </Col>
-        </Row>
-      ) : null;
-    };
-
     return (
       <Modal isOpen contentLabel="modal" onRequestClose={onClose}>
         <div>
@@ -187,7 +161,14 @@ class EditSpentModal extends Component {
                     <Input disabled value={status} />
                   </Col>
                 </Row>
-                {billableRow()}
+                <Row className={css.inputRow}>
+                  <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
+                    <p>Billable:</p>
+                  </Col>
+                  <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
+                    <Checkbox disabled checked={isBillable} onChange={this.changeBillable} />
+                  </Col>
+                </Row>
               </div>
             ) : null}
             <div className={css.buttonWrap}>
