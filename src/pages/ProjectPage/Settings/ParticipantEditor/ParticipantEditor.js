81bf1b1c2163e229387e5f6c3fa5f9bdda1d0532
Anton Bistryakov <anton.bistryakov@simbirsoft.com>
2018-11-09 18:00:35 +0400

    ST-10915 transport create Jira button from projects to settings

--- src/pages/ProjectPage/Settings/ParticipantEditor/ParticipantEditor.js
+++ src/pages/ProjectPage/Settings/ParticipantEditor/ParticipantEditor.js
@@ -16,6 +16,7 @@ import Modal from '../../../../components/Modal';
 import SelectDropdown from '../../../../components/SelectDropdown';
 import localize from './participantEditor.json';
 import layoutAgnosticFilter from '../../../../utils/layoutAgnosticFilter';
+import Wizard from '../../../../components/Wizard';
 
 class ParticipantEditor extends Component {
   constructor(props) {
@@ -24,6 +25,7 @@ class ParticipantEditor extends Component {
     this.state = {
       isModalOpenAddUser: false,
       isModalOpenAddExternal: false,
+      isModalOpenWizard: false,
       participant: null,
       roles: [],
       participants: []
@@ -198,6 +200,18 @@ class ParticipantEditor extends Component {
     this.setState({ isModalOpenAddExternal: true });
   };
 
+  handleOpenModalWizard = () => {
+    this.setState({
+      isModalOpenWizard: true
+    });
+  };
+
+  handleCloseModalWizard = () => {
+    this.setState({
+      isModalOpenWizard: false
+    });
+  };
+
   checkIsPmInProject = () =>
     this.props.users.some(user => {
       if (user.id === this.props.user.id) {
@@ -305,6 +319,15 @@ class ParticipantEditor extends Component {
             />
           ) : null}
         </div>
+        <div className={css.externalUsers}>
+          <h2>{localize[lang].SYNCHRONIZATION_WITH_JIRA}</h2>
+          <Button
+            onClick={this.handleOpenModalWizard}
+            text={localize[lang].CREATE_PROJECT_WITH_JIRA}
+            type="primary"
+            icon="IconPlus"
+          />
+        </div>
         {this.state.isModalOpenAddUser ? (
           <Modal isOpen contentLabel="modal" onRequestClose={this.handleCloseModalAddUser}>
             <form className={css.changeStage}>
@@ -371,6 +394,7 @@ class ParticipantEditor extends Component {
             </form>
           </Modal>
         ) : null}
+        <Wizard lang={lang} isOpen={this.state.isModalOpenWizard} onRequestClose={this.handleCloseModalWizard} />
       </div>
     );
   }
