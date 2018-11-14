36f40abf8c4c05b4566c435cbcda2865e5080513
Sergey Chernov <sergey.chernov@simbirsoft.com>
2018-11-14 16:06:14 +0300

    ST-11014 fix and relevance var name

--- src/components/AgileBoardFilter/FilterForm/FilterForm.js
+++ src/components/AgileBoardFilter/FilterForm/FilterForm.js
@@ -67,10 +67,10 @@ class FilterForm extends React.Component {
     };
   }
 
-  getSprintTime(sprints) {
-    return sprints && sprints.length && this.props.sprints && this.props.sprints.length
-      ? sprints.map(sprint => {
-          const sprintData = this.props.sprints.find(data => data.id === +sprint.value) || {};
+  getSprintTime(sprintIds) {
+    return sprintIds && sprintIds.length && this.props.sprints && this.props.sprints.length
+      ? sprintIds.map(sprintId => {
+          const sprintData = this.props.sprints.find(data => data.id === +sprintId) || {};
           return `${sprintData.spentTime || 0} / ${sprintData.budget || 0}`;
         })
       : [];
