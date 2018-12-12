0e09d575aa108032603b658725f01ed1ea014c5e
Yuri Nikulin <kowkogab@gmail.com>
2018-12-12 14:54:33 +0400

    ST-12710 project settings: console warning fixed on user adding

--- src/pages/ProjectPage/Settings/ParticipantEditor/ParticipantEditor.js
+++ src/pages/ProjectPage/Settings/ParticipantEditor/ParticipantEditor.js
@@ -342,7 +342,7 @@ class ParticipantEditor extends Component {
                   onInputChange={this.searchOnChange}
                   noResultsText={localize[lang].NO_RESULTS}
                   options={this.getUsers()}
-                  autofocus
+                  autoFocus
                   filterOption={el => el}
                 />
                 <SelectDropdown
@@ -380,7 +380,7 @@ class ParticipantEditor extends Component {
                   onInputChange={this.searchExternalOnChange}
                   noResultsText={localize[lang].NO_RESULTS}
                   options={this.getUsers()}
-                  autofocus
+                  autoFocus
                   filterOption={layoutAgnosticFilter}
                 />
                 <Button
