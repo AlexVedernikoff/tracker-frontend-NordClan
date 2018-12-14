9a5f30adee497d71ee6384887fe16e3f3b39b31c
petyankinav <aleksandr.petyankin@simbirsoft.com>
2018-12-14 11:28:11 +0300

    ST-12776 Fix long wait for task update

--- src/actions/Task.js
+++ src/actions/Task.js
@@ -60,14 +60,10 @@ const requestTaskChange = () => ({
   type: TaskActions.TASK_CHANGE_REQUEST_SENT
 });
 
-// const successTaskChange = changedFields => ({
-//   type: TaskActions.TASK_CHANGE_REQUEST_SUCCESS,
-//   changedFields
-// });
-
-// const requestTaskChangeUser = () => ({
-//   type: TaskActions.TASK_CHANGE_REQUEST_SENT
-// });
+const successTaskChange = changedFields => ({
+  type: TaskActions.TASK_CHANGE_REQUEST_SUCCESS,
+  changedFields
+});
 
 // const successTaskChangeUser = changedFields => ({
 //   type: TaskActions.TASK_CHANGE_USER_SUCCESS,
@@ -305,21 +301,26 @@ const changeTask = (ChangedProperties, target, callback) => {
     return;
   }
   return dispatch => {
-    dispatch({
-      type: REST_API,
-      url: `/task/${ChangedProperties.id}`,
-      method: PUT,
-      body: ChangedProperties,
-      extra,
-      start: withStartLoading(requestTaskChange, true)(dispatch),
-      response: withFinishLoading(() => {
-        if (callback) {
-          callback();
+    const URL = `${API_URL}/task/${ChangedProperties.id}`;
+    dispatch(startLoading());
+    axios
+      .put(URL, ChangedProperties)
+      .then(res => {
+        console.log(res);
+        if (res.data.length) {
+          res.data.forEach(task => dispatch(successTaskChange(task)));
+          if (callback) {
+            callback();
+          }
         }
-        return stopTaskEditing(target);
-      }, true)(dispatch),
-      error: defaultErrorHandler(dispatch(stopTaskEditing(target)))
-    });
+        dispatch(finishLoading());
+        stopTaskEditing(target);
+      })
+      .catch(() => {
+        defaultErrorHandler(dispatch);
+        stopTaskEditing(target);
+        dispatch(finishLoading());
+      });
   };
 };
 
