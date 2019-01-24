bb273d2fd05d2c72f7eaaef5bfd05c825de2da39
Chapurin <chapurin.vladimir.v@yandex.ru>
2019-01-24 14:23:33 +0400

    ST-11012 fix sso https

--- src/utils/keycloak.js
+++ src/utils/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 import { setCookies } from './cookies';
 
 const keycloak = Keycloak({
-  url: process.env.KEYCLOAK_URL || 'http://sso.simbirsoft:8080/auth',
+  url: process.env.KEYCLOAK_URL || 'https://sso.simbirsoft:8080/auth',
   realm: process.env.KEYCLOAK_REALM || 'simbirsoft-dev',
   clientId: process.env.KEYCLOAK_CLIENTID || 'local-simtrack-public'
 });
