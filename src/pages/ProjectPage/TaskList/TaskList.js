0863c9e76412db53a7c3389b3bb691e4d6154c6e
petyankinav <aleksandr.petyankin@simbirsoft.com>
2019-02-14 17:06:24 +0300

    ST-16826 Fix default sprint value

--- src/pages/ProjectPage/TaskList/TaskList.js
+++ src/pages/ProjectPage/TaskList/TaskList.js
@@ -130,7 +130,8 @@ class TaskList extends Component {
   makeFiltersObject = (name, value) => {
     const { project } = this.props;
     const { sprints } = project;
-    const currentSprint = sprints ? sprints.filter(item => item.statusId === 2)[0].id : 0;
+    let currentSprint = sprints ? sprints.filter(item => item.statusId === 2)[0] : 0;
+    currentSprint = currentSprint ? currentSprint.id : 0;
     let processedValue;
     const defaultValue = emptyFilters[name];
     if (['sprintId', 'performerId', 'statusId', 'typeId', 'tags'].indexOf(name) !== -1) {
