9dafc1014d6bad8e4e6577d234999af5ae94ec13
Yuri Nikulin <kowkogab@gmail.com>
2019-03-26 13:52:41 +0400

    ST-19058 hide tooltip after editGoal button press

--- src/pages/ProjectPage/RoadMap/Sprint/Sprint.js
+++ src/pages/ProjectPage/RoadMap/Sprint/Sprint.js
@@ -67,7 +67,10 @@ class Sprint extends Component {
 
   transferGoal = (goalId, createdAt) => () => this.props.transfer(goalId, createdAt);
 
-  hanldeEditSprint = () => this.props.editSprint(this.props.item);
+  hanldeEditSprint = () => {
+    ReactTooltip.hide();
+    this.props.editSprint(this.props.item);
+  };
 
   toggleVisible = (id, visible) => () => this.props.toggleVisible(id, !visible);
 
