f4ef640a47ec0218f040a3cfd08e34fee460c37b
Inna <inna.shutova@nordclan.com>
2019-12-23 14:02:38 +0400

    fix problem with saving comments

--- src/pages/Timesheets/ActivityRow/TotalComment/TotalComment.js
+++ src/pages/Timesheets/ActivityRow/TotalComment/TotalComment.js
@@ -27,7 +27,7 @@ class TotalComment extends React.Component {
     const updatedComments = {};
     if (props.items.length) {
       props.items.forEach(tsh => {
-        if (tsh.id) updatedComments[tsh.id] = tsh.comment;
+        if (tsh.id) updatedComments[tsh.id] = tsh.comment !== null && tsh.comment !== undefined ? tsh.comment : '';
       });
     }
     this.state = {
