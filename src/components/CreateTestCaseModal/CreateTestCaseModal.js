c7b4336b1e70d313acf8537abb98ac14172a3559
Rishat Mukhametgaliev <mrishat03@gmail.com>
2020-05-08 16:52:41 +0300

    wip class impl

--- src/components/CreateTestCaseModal/CreateTestCaseModal.js
+++ src/components/CreateTestCaseModal/CreateTestCaseModal.js
@@ -2,43 +2,67 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
-import * as css from './CreateTestCaseModal.scss';
-import localize from './CreateTestCaseModal.json';
-import rules from './constants';
+import { stateToHTML } from 'draft-js-export-html';
+import TimePicker from 'rc-time-picker';
+import moment from 'moment';
+
 import Modal from '../../components/Modal';
 import Validator from '../../components/ValidatedInput/Validator';
 import ValidatedAutosizeInput from '../ValidatedAutosizeInput';
+import ValidatedTextEditor from '../ValidatedTextEditor';
+
+import * as css from './CreateTestCaseModal.scss';
+import localize from './CreateTestCaseModal.json';
+import rules from './constants';
+import { TEST_TASK_STATUSES } from '../../constants/TestTaskStatus';
 
 class CreateTestCaseModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ''
+      title: '',
+      description: '',
+      priority: 3,
+      preConditions: '',
+      postConditions: '',
+      status: TEST_TASK_STATUSES.ACTUAL,
+      steps: [{ action: '', expectedResult: '' }],
+      duration: '00:10:00'
     };
     this.validator = new Validator();
-    this.fieldNames = Object.keys(this.state);
   }
 
   handleChange = field => event => {
     this.setState({ [field]: event.target.value.trim() });
   };
 
+  handleTextEditorChange = field => editorState => {
+    this.setState({ [field]: stateToHTML(editorState.getCurrentContent()) });
+  };
+
+  validateAndSubmit = event => {
+    event.preventDefault();
+    console.log('validated');
+  };
+
   getFieldError = fieldName => {
     const { lang } = this.props;
     switch (fieldName) {
-      case this.fieldNames.title:
+      case 'title':
         const { title } = this.state;
         return title > rules.MIN_TITLE_LENGTH
           ? localize[lang].TITLE_ERROR.TOO_LONG
           : localize[lang].TITLE_ERROR.TOO_SHORT;
+      case 'description':
+        return localize[lang].TEXT_ERROR_TOO_LONG;
       default:
         return '';
     }
   };
 
   render() {
-    const { onCancel, closeTimeoutMS, lang, ...other } = this.props;
-    const { title } = this.state;
+    const { onCancel, closeTimeoutMS, isOpen, lang, ...other } = this.props;
+    const { title, description, duration } = this.state;
 
     const formLayout = {
       firstCol: 4,
@@ -46,32 +70,58 @@ class CreateTestCaseModal extends Component {
     };
 
     return (
-      <Modal {...other} onRequestClose={onCancel} closeTimeoutMS={200 || closeTimeoutMS}>
+      <Modal {...other} isOpen={isOpen} onRequestClose={onCancel} closeTimeoutMS={200 || closeTimeoutMS}>
         <form className={css.container}>
           <h3>{localize[lang].FORM_TITLE}</h3>
           <hr />
-          <label className={css.formField}>
+          <label className={css.field}>
             <Row>
-              <Col xs={12} sm={formLayout.firstCol} className={css.leftColumn}>
-                <p>{localize[lang].TITLE} </p>
+              <Col xs={12} sm={formLayout.firstCol} className={css.label}>
+                <p>{localize[lang].TITLE_LABEL} </p>
               </Col>
-              <Col xs={12} sm={formLayout.secondCol} className={css.rightColumn}>
+              <Col xs={12} sm={formLayout.secondCol} className={css.fieldInput}>
                 {this.validator.validate(
                   (handleBlur, shouldMarkError) => (
                     <ValidatedAutosizeInput
                       maxRows={5}
                       autoFocus
-                      name={this.fieldNames.title}
+                      name="title"
                       placeholder={localize[lang].TITLE_PLACEHOLDER}
-                      onChange={this.handleChange(this.fieldNames.title)}
+                      onChange={this.handleChange('title')}
                       onBlur={handleBlur}
-                      onEnter={this.validateAndSubmit}
+                      onSubmit={this.validateAndSubmit}
                       shouldMarkError={shouldMarkError}
-                      errorText={this.generateError(this.fieldNames.title)}
+                      errorText={this.getFieldError('title')}
                     />
                   ),
-                  this.fieldNames.title,
+                  'title',
                   title.length < rules.MIN_TITLE_LENGTH || title.length > rules.MAX_TITLE_LENGTH
+                )}
+              </Col>
+            </Row>
+          </label>
+          <label className={css.field}>
+            <Row>
+              <Col xs={12} sm={formLayout.firstCol} className={css.label}>
+                <p>{localize[lang].DESCRIPTION_LABEL}</p>
+              </Col>
+              <Col xs={12} sm={formLayout.secondCol} className={css.fieldInput}>
+                {this.validator.validate(
+                  (handleBlur, shouldMarkError) => (
+                    <ValidatedTextEditor
+                      toolbarHidden
+                      onEditorStateChange={this.handleTextEditorChange('description')}
+                      placeholder={localize[lang].DESCRIPTION_PLACEHOLDER}
+                      wrapperClassName={css.descriptionWrapper}
+                      editorClassName={css.description}
+                      onBlur={handleBlur}
+                      content={''}
+                      shouldMarkError={shouldMarkError}
+                      errorText={this.getFieldError('description')}
+                    />
+                  ),
+                  'description',
+                  description.length > rules.MAX_TEXT_LENGTH
                 )}
               </Col>
             </Row>
@@ -84,7 +134,15 @@ class CreateTestCaseModal extends Component {
 
 CreateTestCaseModal.propTypes = {
   closeTimeoutMS: PropTypes.number,
-  onCancel: PropTypes.func.isRequired
+  isOpen: PropTypes.bool,
+  lang: PropTypes.string,
+  onCancel: PropTypes.func
+};
+
+CreateTestCaseModal.defaultProps = {
+  isOpen: true,
+  lang: 'en',
+  onCancel: () => console.log('canceled')
 };
 
 const mapStateToProps = state => ({ lang: state.Localize.lang });
