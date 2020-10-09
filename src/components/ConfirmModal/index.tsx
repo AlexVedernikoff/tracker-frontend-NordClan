f42734038a53325c9264f5b76f2724cebe92d19a
aleksandr.korotaev <alexander.korotaev@nordclan.com>
2020-10-09 16:49:09 +0300

    TT-9224 Use project confirm message for delete test-case

--- src/components/ConfirmModal/index.tsx
+++ src/components/ConfirmModal/index.tsx
@@ -1,9 +1,14 @@
 import ConfirmModal from './ConfirmModal';
+import useConfirmModal from './ConfirmModalHook';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
   lang: state.Localize.lang
 });
+
+export {
+  useConfirmModal
+};
 
 export default connect(
   mapStateToProps,
