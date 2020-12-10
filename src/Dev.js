15277721615f1b322dbbcb555025767f2deaabf0
aleksandr.korotaev <alexander.korotaev@nordclan.com>
2020-12-10 23:15:09 +0300

    Errors boundary component

--- src/Dev.js
+++ src/Dev.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoute from './Router';
+import ErrorBoundary from './components/ErrorBoundary';
 import 'mobx-react-lite/batchingForReactDom';
 
 import 'normalize.css';
@@ -35,9 +36,11 @@ const render = App => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <DragDropContextProvider backend={HTML5Backend}>
-          <App history={history} socket={socket} />
-        </DragDropContextProvider>
+        <ErrorBoundary>
+          <DragDropContextProvider backend={HTML5Backend}>
+            <App history={history} socket={socket} />
+          </DragDropContextProvider>
+        </ErrorBoundary>
       </Provider>
     </AppContainer>,
     rootEl
