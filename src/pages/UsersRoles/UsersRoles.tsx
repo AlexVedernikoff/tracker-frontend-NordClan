27bc6c0c9ac5a511ffe59d558901f4f6d3abf994
janevermilion <janevermilion@gmail.com>
2022-03-29 14:13:30 +0300

    TR-21564.Перепутаны местами поля 'Год рождения' и 'Дата трудоустройства'

--- src/pages/UsersRoles/UsersRoles.tsx
+++ src/pages/UsersRoles/UsersRoles.tsx
@@ -162,13 +162,13 @@ class UsersRoles extends React.Component<any, any> {
           </a>
         </td>
         <td>
-          {toIso(employmentDate)}
+          {toIso(birthDate)}
         </td>
         <td>
           {city}
         </td>
         <td>
-          {toIso(birthDate)}
+          {toIso(employmentDate)}
         </td>
         <td>
           {telegram}
