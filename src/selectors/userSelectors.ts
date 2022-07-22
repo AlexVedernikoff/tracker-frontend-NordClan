44570d665345ade99f6e7debe99014689283881e
trofim ivanchenko <trofim.ivanchenko@nordclan.com>
2022-07-22 16:49:47 +0300

    code style fix

--- src/selectors/userSelectors.ts
+++ src/selectors/userSelectors.ts
@@ -1,28 +1,27 @@
 import { createSelector } from 'reselect';
 import checkRoles from '../utils/checkRoles';
 
-
 const userListSelector = state => state.UserList;
 export const userSelector = createSelector([userListSelector], state => {
-	return state.user;
+  return state.user;
 });
 
 const authUserSelector = ({ state }) => state.Auth.user;
 const paramsFromPropsSelector = ({ props }) => props.params;
 export const userIdSelector = createSelector<any, any, any, any>([authUserSelector, paramsFromPropsSelector], (user, params) => {
-	const userId = 'id' in params ? params.id : user.id;
-	return String(userId);
+  const userId = 'id' in params ? params.id : user.id;
+  return String(userId);
 });
 
 const langSelector = ({ state }) => state.Localize.lang;
 const multilingualDictionarySelector = ({ multilingualDictionary }) => {
-	return multilingualDictionary;
+  return multilingualDictionary;
 };
 export const dictionarySelector = createSelector<any, any, any, any>(
-	[langSelector, multilingualDictionarySelector],
-	(lang, multilingualDictionary) => {
-		return multilingualDictionary[lang];
-	}
+  [langSelector, multilingualDictionarySelector],
+  (lang, multilingualDictionary) => {
+    return multilingualDictionary[lang];
+  }
 );
 
 export const globalRole = createSelector([authUserSelector], user => user.globalRole);
