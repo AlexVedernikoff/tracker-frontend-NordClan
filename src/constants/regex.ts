4031106558a8e0c0df6e88c89d8c3495e7974355
David Bdoyan <david.bdoyan@nordclan.com>
2023-04-10 13:53:13 +0300

    feat(ui): Add telegram nickname regex

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,3 +1,5 @@
 export const EN_SYMBOLS_REGEX = /[a-zA-Z\\0-9\\!#$%+\(\)\*\.~_=`]/g;
 
 export const RU_SYMBOLS_REGEX = /[А-яЁё\\0-9\\!#$%+\(\)\*\.~_=`]/g;
+
+export const TELEGRAM_SYMBOLS_REGEX = /^(?=.*[a-zA-Z])+(?=.*[0-9])*(?=.*_)*[A-z0-9_]{5,}$/;
